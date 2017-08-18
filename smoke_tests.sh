93f96dbee68376184dd1cb9040915ac64da9d28c
Giorgio Sironi <g.sironi@elifesciences.org>
2017-08-18 11:36:31 +0100

    Smoke testing through nginx

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -2,5 +2,10 @@
 . /opt/smoke.sh/smoke.sh
 
 smoke_url_ok $(hostname):3000/
+if [ "$ENVIRONMENT_NAME" == 'dev' ]; then
+    smoke_url_ok http://$(hostname)/
+else
+    smoke_url_ok https://$(hostname)/
+fi
 
 smoke_report
