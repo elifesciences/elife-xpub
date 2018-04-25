9d43cafb06d55c80f939c1d9a5e65c35b770afe8
Samuel Galson <samgalson@gmail.com>
2018-04-25 15:21:47 +0100

    feat(submission): update formik schema

--- app/components/submission/AuthorDetails/AuthorDetailsSchema.js
+++ app/components/submission/AuthorDetails/AuthorDetailsSchema.js
@@ -9,22 +9,23 @@ const email = () =>
     .required('Email is required')
     .email('Must be a valid email address')
 
-const institution = () => yup.string().required('Institution is required')
+const institution = () => yup.string().required('Institute is required')
 
 const schema = yup.object().shape({
-  firstName: firstName(),
-  lastName: lastName(),
-  email: email(),
-  institution: institution(),
-  assignee: yup
-    .object()
-    .nullable()
-    .shape({
+  submissionMeta: yup.object().shape({
+    displayCorrespondent: yup.boolean(),
+    author: yup.object().shape({
       firstName: firstName(),
       lastName: lastName(),
       email: email(),
       institution: institution(),
     }),
+    correspondent: yup.object().shape({
+      firstName: firstName(),
+      lastName: lastName(),
+      email: email(),
+    }),
+  }),
 })
 
 const empty = {
