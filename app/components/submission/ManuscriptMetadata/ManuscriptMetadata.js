47d5262cd378970258149ed4fbfacb80e870cb27
Jennifer Spencer <jennifer.spencer@yld.io>
2018-05-01 17:33:11 +0100

    refactor(manuscriptMetadata): abstract CalloutGeneratedChild pattern into react component

--- app/components/submission/ManuscriptMetadata/ManuscriptMetadata.js
+++ app/components/submission/ManuscriptMetadata/ManuscriptMetadata.js
@@ -2,12 +2,32 @@ import React from 'react'
 
 import { H1, Menu, Checkbox, Button } from '@pubsweet/ui'
 import { Field } from 'formik'
+import { get } from 'lodash'
 
 import ValidatedField from '../../ui/atoms/ValidatedField'
 import CalloutBox from '../../ui/atoms/CalloutBox'
 import ButtonLink from '../../ui/atoms/ButtonLink'
 import Textarea from '../../ui/atoms/Textarea'
 import ProgressBar from '../ProgressBar'
+
+const CheckboxGeneratedChild = ({
+  values,
+  fieldName,
+  checkboxStatus,
+  checkboxLabel,
+  children,
+  ...props
+}) => (
+  <CalloutBox enabled={get(values, fieldName)}>
+    <Field
+      name={fieldName}
+      render={({ field }) => (
+        <Checkbox {...field} checked={field.value} label={checkboxLabel} />
+      )}
+    />
+    {get(values, fieldName) && children}
+  </CalloutBox>
+)
 
 const ManuscriptMetadata = ({ handleSubmit, values, setFieldValue }) => (
   <form noValidate onSubmit={handleSubmit}>
@@ -28,66 +48,42 @@ const ManuscriptMetadata = ({ handleSubmit, values, setFieldValue }) => (
       ]}
     />
 
-    <CalloutBox enabled={values.metadata.discussedPreviously}>
-      <Field
-        name="metadata.discussedPreviously"
-        render={({ field }) => (
-          <Checkbox
-            {...field}
-            checked={field.value}
-            label="This manuscript has been discussed previously with an eLife editor"
-          />
-        )}
+    <CheckboxGeneratedChild
+      checkboxLabel="This manuscript has been discussed previously with an eLife editor"
+      fieldName="metadata.discussedPreviously"
+      values={values}
+    >
+      <ValidatedField
+        component={Textarea}
+        label="What was discussed and with whom"
+        name="metadata.discussion"
       />
-      {values.metadata.discussedPreviously && (
-        <ValidatedField
-          component={Textarea}
-          label="What was discussed and with whom"
-          name="metadata.discussion"
-        />
-      )}
-    </CalloutBox>
+    </CheckboxGeneratedChild>
 
-    <CalloutBox enabled={values.metadata.consideredPreviously}>
-      <Field
-        name="metadata.consideredPreviously"
-        render={({ field }) => (
-          <Checkbox
-            {...field}
-            checked={field.value}
-            label="This manuscript has been considered by eLife previously"
-          />
-        )}
+    <CheckboxGeneratedChild
+      checkboxLabel="This manuscript has been considered by eLife previously"
+      fieldName="metadata.consideredPreviously"
+      values={values}
+    >
+      <ValidatedField
+        label="Article title/reference No"
+        name="metadata.previousArticle"
       />
-      {values.metadata.consideredPreviously && (
-        <ValidatedField
-          label="Article title/reference No"
-          name="metadata.previousArticle"
-        />
-      )}
-    </CalloutBox>
+    </CheckboxGeneratedChild>
 
-    <CalloutBox enabled={values.metadata.cosubmission}>
-      <Field
-        name="metadata.cosubmission"
-        render={({ field }) => (
-          <Checkbox
-            {...field}
-            checked={field.value}
-            label="This manuscript is a co-submission"
-          />
-        )}
-      />
-      {values.metadata.cosubmission && (
-        <div>
-          <ValidatedField
-            label="Article title"
-            name="metadata.cosubmissionTitle"
-          />
-          <ValidatedField label="Reference No" name="metadata.cosubmissionId" />
-        </div>
-      )}
-    </CalloutBox>
+    <CheckboxGeneratedChild
+      checkboxLabel="This manuscript is a co-submission"
+      fieldName="metadata.cosubmission"
+      values={values}
+    >
+      <div>
+        <ValidatedField
+          label="Article title"
+          name="metadata.cosubmissionTitle"
+        />
+        <ValidatedField label="Reference No" name="metadata.cosubmissionId" />
+      </div>
+    </CheckboxGeneratedChild>
 
     <Button data-test-id="next" primary type="submit">
       Next
