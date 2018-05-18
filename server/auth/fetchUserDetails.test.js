9698bbf3e4ce7f72ba2324a03c8aa139bd5457f4
Jennifer Spencer <jennifer.spencer@yld.io>
2018-05-18 17:17:59 +0100

    test: intercept the request to orcid for fetching user details with replay

--- server/auth/fetchUserDetails.test.js
+++ server/auth/fetchUserDetails.test.js
@@ -1,9 +1,10 @@
-require('../../test/helpers/replay-setup')
+const replaySetup = require('../../test/helpers/replay-setup')
 
 const fetchUserDetails = require('./fetchUserDetails')
 
 describe('Fetch ORCID author details', () => {
   it('gets user details', async () => {
+    replaySetup('success')
     const user = {
       orcid: '0000-0003-3146-0256',
       oauth: { accessToken: 'f7617529-f46a-40b1-99f4-4181859783ca' },
