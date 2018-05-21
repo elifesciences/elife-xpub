f12b73c1c71a5a485c373938bff4cb6d4a0ababf
Jennifer Spencer <jennifer.spencer@yld.io>
2018-05-21 14:10:29 +0100

    refactor: change notation from slashes to surrounding speech marks

--- test/pageObjects/author-details.js
+++ test/pageObjects/author-details.js
@@ -2,10 +2,10 @@ import config from 'config'
 
 const authorDetails = {
   url: `${config.get('pubsweet-server.baseUrl')}/submit`,
-  firstNameField: '[name=submissionMeta\\.author\\.firstName]',
-  secondNameField: '[name=submissionMeta\\.author\\.lastName]',
-  emailField: '[name=submissionMeta\\.author\\.email]',
-  institutionField: '[name=submissionMeta\\.author\\.institution]',
+  firstNameField: '[name="submissionMeta.author.firstName"]',
+  secondNameField: '[name="submissionMeta.author.lastName"]',
+  emailField: '[name="submissionMeta.author.email"]',
+  institutionField: '[name="submissionMeta.author.institution"]',
 }
 
 export default authorDetails
