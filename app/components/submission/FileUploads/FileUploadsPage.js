cf0cb3f583e27058dd0007c4bcdd35207a167545
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-05-24 18:19:01 +0100

    refactor: move submission and form logic up to submission page
    Reduces duplication and paves the way for redirecting the user to the last valid page.

--- app/components/submission/FileUploads/FileUploadsPage.js
+++ app/components/submission/FileUploads/FileUploadsPage.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
-import omitDeep from 'omit-deep-lodash'
-import { Formik } from 'formik'
 import FileUploads from './FileUploads'
-import { schema } from './FileUploadsSchema'
-import WithCurrentSubmission from '../WithCurrentSubmission'
 
 const UPLOAD_MUTATION = gql`
   mutation UploadFile($id: ID!, $file: Upload!) {
@@ -20,55 +16,42 @@ const UPLOAD_MUTATION = gql`
   }
 `
 
-const FileUploadsPage = ({ history }) => (
-  <WithCurrentSubmission>
-    {(updateSubmission, initialValues) => (
-      <Mutation mutation={UPLOAD_MUTATION}>
-        {(uploadFile, { data: uploadData, loading, error: uploadError }) => (
-          <Formik
-            initialValues={initialValues}
-            onSubmit={(values, { setSubmitting, setErrors }) => {
-              const data = omitDeep(values, ['__typename', 'files'])
-              return updateSubmission({ variables: { data } })
-                .then(() => setSubmitting(false))
-                .then(() => history.push('/submit/metadata'))
-                .catch(errors => {
-                  setErrors(errors)
-                })
-            }}
-            validationSchema={schema}
-          >
-            {({ setFieldValue, errors, touched, values, ...props }) => {
-              const fieldName = 'files'
-              return (
-                <FileUploads
-                  conversion={{
-                    converting: loading,
-                    // TODO import this constant from somewhere (data model package?)
-                    completed: values[fieldName].includes(
-                      file => file.type === 'MANUSCRIPT_SOURCE',
-                    ),
-                    error: uploadError,
-                  }}
-                  formError={errors[fieldName] && touched[fieldName]}
-                  onDrop={([file]) =>
-                    uploadFile({
-                      variables: { file, id: initialValues.id },
-                    }).then(({ data }) => {
-                      setFieldValue('title', data.uploadManuscript.title)
-                      setFieldValue(fieldName, data.uploadManuscript.files)
-                    })
-                  }
-                  setFieldValue={setFieldValue}
-                  {...props}
-                />
-              )
-            }}
-          </Formik>
-        )}
-      </Mutation>
-    )}
-  </WithCurrentSubmission>
+const FileUploadsPage = ({
+  manuscriptId,
+  setFieldValue,
+  errors,
+  touched,
+  values,
+  ...props
+}) => (
+  <Mutation mutation={UPLOAD_MUTATION}>
+    {(uploadFile, { loading, error: uploadError }) => {
+      const fieldName = 'files'
+      return (
+        <FileUploads
+          conversion={{
+            converting: loading,
+            // TODO import this constant from somewhere (data model package?)
+            completed: values[fieldName].includes(
+              file => file.type === 'MANUSCRIPT_SOURCE',
+            ),
+            error: uploadError,
+          }}
+          formError={errors[fieldName] && touched[fieldName]}
+          onDrop={([file]) =>
+            uploadFile({
+              variables: { file, id: values.id },
+            }).then(({ data }) => {
+              setFieldValue('title', data.uploadManuscript.title)
+              setFieldValue(fieldName, data.uploadManuscript.files)
+            })
+          }
+          setFieldValue={setFieldValue}
+          {...props}
+        />
+      )
+    }}
+  </Mutation>
 )
 
 export default FileUploadsPage
--- app/components/submission/SubmissionPage.js
+++ app/components/submission/SubmissionPage.js
@@ -1,20 +1,90 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import AuthorDetailsPage from './AuthorDetails/AuthorDetailsPage'
+import { Formik } from 'formik'
+import omitDeep from 'omit-deep-lodash'
+import WithCurrentSubmission from './WithCurrentSubmission'
+import AuthorDetails from './AuthorDetails/AuthorDetails'
 import FileUploadsPage from './FileUploads/FileUploadsPage'
-import ManuscriptMetadataPage from './ManuscriptMetadata/ManuscriptMetadataPage'
-import ReviewerSuggestionsPage from './ReviewerSuggestions/ReviewerSuggestionsPage'
+import ManuscriptMetadata from './ManuscriptMetadata/ManuscriptMetadata'
+import ReviewerSuggestions from './ReviewerSuggestions/ReviewerSuggestions'
+import { schema as fileUploadsSchema } from './FileUploads/FileUploadsSchema'
+import { schema as authorDetailsSchema } from './AuthorDetails/AuthorDetailsSchema'
+import { schema as manuscriptMetadataSchema } from './ManuscriptMetadata/ManuscriptMetadataSchema'
+import { schema as reviewerSuggestionsSchema } from './ReviewerSuggestions/ReviewerSuggestionsSchema'
 
-const SubmissionPage = ({ match }) => (
-  <Switch>
-    <Route component={FileUploadsPage} path={`${match.path}/upload`} />
-    <Route component={ManuscriptMetadataPage} path={`${match.path}/metadata`} />
-    <Route
-      component={ReviewerSuggestionsPage}
-      path={`${match.path}/suggestions`}
-    />
-    <Route component={AuthorDetailsPage} />
-  </Switch>
+const FormStep = ({ history, updateSubmission, nextUrl, ...props }) => (
+  <Formik
+    onSubmit={(values, { setSubmitting, setErrors }) => {
+      const data = omitDeep(values, ['__typename', 'files'])
+      return updateSubmission({ variables: { data } })
+        .then(() => setSubmitting(false))
+        .then(() => history.push(nextUrl))
+        .catch(errors => {
+          setErrors(errors)
+        })
+    }}
+    {...props}
+  />
+)
+
+const SubmissionPage = ({ match, history }) => (
+  <WithCurrentSubmission>
+    {(updateSubmission, initialValues) => (
+      <Switch>
+        <Route
+          path={`${match.path}/upload`}
+          render={() => (
+            <FormStep
+              component={FileUploadsPage}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`${match.path}/metadata`}
+              updateSubmission={updateSubmission}
+              validationSchema={fileUploadsSchema}
+            />
+          )}
+        />
+        <Route
+          path={`${match.path}/metadata`}
+          render={() => (
+            <FormStep
+              component={ManuscriptMetadata}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`${match.path}/suggestions`}
+              updateSubmission={updateSubmission}
+              validationSchema={manuscriptMetadataSchema}
+            />
+          )}
+        />
+        <Route
+          path={`${match.path}/suggestions`}
+          render={() => (
+            <FormStep
+              component={ReviewerSuggestions}
+              history={history}
+              initialValues={initialValues}
+              nextUrl="/dashboard"
+              updateSubmission={updateSubmission}
+              validationSchema={reviewerSuggestionsSchema}
+            />
+          )}
+        />
+        <Route
+          render={() => (
+            <FormStep
+              component={AuthorDetails}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`${match.path}/upload`}
+              updateSubmission={updateSubmission}
+              validationSchema={authorDetailsSchema}
+            />
+          )}
+        />
+      </Switch>
+    )}
+  </WithCurrentSubmission>
 )
 
 export default SubmissionPage
