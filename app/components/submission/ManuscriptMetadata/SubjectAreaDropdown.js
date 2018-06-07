7f5028459b62d60a330e5d03d478a157bec31563
Jennifer Spencer <jennifer.spencer@yld.io>
2018-06-07 13:07:56 +0100

    feat(SubjectAreaDropdown): limit number of items selected from dropdown to 2

--- app/components/submission/ManuscriptMetadata/SubjectAreaDropdown.js
+++ app/components/submission/ManuscriptMetadata/SubjectAreaDropdown.js
@@ -16,6 +16,7 @@ class ThemelessSubjectAreaDropdown extends React.Component {
 
     this.state = {
       selectedOptions: [],
+      hasReachedMultiselectLimit: false,
     }
 
     this.handleChange = this.handleChange.bind(this)
@@ -32,8 +33,13 @@ class ThemelessSubjectAreaDropdown extends React.Component {
   // https://github.com/JedWatson/react-select/blob/v2/src/Select.js
   // react-select exposes onChange: (ValueType, ActionMeta) => void
   handleChange = selectedOptions => {
-    this.setState({ selectedOptions })
-    if (this.props.onChange) this.props.onChange(selectedOptions)
+    if (selectedOptions.length <= 2) {
+      this.setState({ selectedOptions, hasReachedMultiselectLimit: false })
+      if (this.props.onChange) this.props.onChange(selectedOptions)
+    }
+    if (selectedOptions.length >= 2) {
+      this.setState({ hasReachedMultiselectLimit: true })
+    }
   }
 
   handleBlur = e => {
@@ -41,73 +47,79 @@ class ThemelessSubjectAreaDropdown extends React.Component {
   }
 
   render() {
-    const { selectedOptions } = this.state
+    const { selectedOptions, hasReachedMultiselectLimit } = this.state
     const { label, name } = this.props
+
+    const selectChildProps = {
+      components: { ClearIndicator: null },
+      inputId: 'subject-area-select',
+      isMulti: true,
+      name,
+      onBlur: this.handleBlur,
+      onChange: this.handleChange,
+      options: [
+        {
+          value: 'structural-biology-molecular-biophysics',
+          label: 'Structural Biology and Molecular Biophysics',
+        },
+        { value: 'plant-biology', label: 'Plant Biology' },
+        { value: 'physics-living-systems', label: 'Physics of Living Systems' },
+        { value: 'neuroscience', label: 'Neuroscience' },
+        {
+          value: 'microbiology-infectious-disease',
+          label: 'Microbiology and Infectious Disease',
+        },
+        {
+          value: 'immunology-inflammation',
+          label: 'Immunology and Inflammation',
+        },
+        {
+          value: 'human-biology-medicine',
+          label: 'Human Biology and Medicine',
+        },
+        { value: 'genetics-genomics', label: 'Genetics and Genomics' },
+        { value: 'evolutionary-biology', label: 'Evolutionary Biology' },
+        {
+          value: 'epidemiology-global-health',
+          label: 'Epidemiology and Global Health',
+        },
+        { value: 'ecology', label: 'Ecology' },
+        {
+          value: 'developmental-biology-stem-cells',
+          label: 'Developmental Biology and Stem Cells',
+        },
+        {
+          value: 'computational-systems-biology',
+          label: 'Computational and Systems Biology',
+        },
+        {
+          value: 'chromosomes-gene-expression',
+          label: 'Chromosomes and Gene Expression',
+        },
+        { value: 'cell-biology', label: 'Cell Biology' },
+        { value: 'cancer-biology', label: 'Cancer Biology' },
+        {
+          value: 'biochemistry-chemical-biology',
+          label: 'Biochemistry and Chemical Biology',
+        },
+      ],
+      styles: this.customReactSelectStyles,
+      value: selectedOptions,
+    }
 
     return (
       <Root>
         {/* htmlFor matches with react-select's inputId, which applies the correct id to the internal sub-component */}
         {/* eslint-disable-next-line jsx-a11y/label-has-for */}
         <label htmlFor="subject-area-select">{label}</label>
-        <Select
-          components={{ ClearIndicator: null }}
-          inputId="subject-area-select"
-          isMulti
-          name={name}
-          onBlur={this.handleBlur}
-          onChange={this.handleChange}
-          options={[
-            {
-              value: 'structural-biology-molecular-biophysics',
-              label: 'Structural Biology and Molecular Biophysics',
-            },
-            { value: 'plant-biology', label: 'Plant Biology' },
-            {
-              value: 'physics-living-systems',
-              label: 'Physics of Living Systems',
-            },
-            { value: 'neuroscience', label: 'Neuroscience' },
-            {
-              value: 'microbiology-infectious-disease',
-              label: 'Microbiology and Infectious Disease',
-            },
-            {
-              value: 'immunology-inflammation',
-              label: 'Immunology and Inflammation',
-            },
-            {
-              value: 'human-biology-medicine',
-              label: 'Human Biology and Medicine',
-            },
-            { value: 'genetics-genomics', label: 'Genetics and Genomics' },
-            { value: 'evolutionary-biology', label: 'Evolutionary Biology' },
-            {
-              value: 'epidemiology-global-health',
-              label: 'Epidemiology and Global Health',
-            },
-            { value: 'ecology', label: 'Ecology' },
-            {
-              value: 'developmental-biology-stem-cells',
-              label: 'Developmental Biology and Stem Cells',
-            },
-            {
-              value: 'computational-systems-biology',
-              label: 'Computational and Systems Biology',
-            },
-            {
-              value: 'chromosomes-gene-expression',
-              label: 'Chromosomes and Gene Expression',
-            },
-            { value: 'cell-biology', label: 'Cell Biology' },
-            { value: 'cancer-biology', label: 'Cancer Biology' },
-            {
-              value: 'biochemistry-chemical-biology',
-              label: 'Biochemistry and Chemical Biology',
-            },
-          ]}
-          styles={this.customReactSelectStyles}
-          value={selectedOptions}
-        />
+        {!hasReachedMultiselectLimit && <Select {...selectChildProps} />}
+        {hasReachedMultiselectLimit && (
+          <Select
+            {...selectChildProps}
+            components={{ ClearIndicator: null, DropdownIndicator: null }}
+            menuIsOpen={false}
+          />
+        )}
       </Root>
     )
   }
