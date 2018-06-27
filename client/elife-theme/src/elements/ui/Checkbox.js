e27ded4d32c9c89175d3db7034821b3b772b824a
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-06-27 16:27:16 +0100

    style: update Checkbox component
    Reorganise theme override folder structure to reflect object structure
    Add Checkmark icon component but use inline data uri until icons are supported in themes

---
+++ client/elife-theme/src/elements/ui/Checkbox.js
@@ -0,0 +1,39 @@
+import { css } from 'styled-components'
+import { th } from '@pubsweet/ui-toolkit'
+
+// TODO: remove this once themes support icons
+const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" fill="#fff" stroke="#fff" />
+</svg>`
+
+export default css`
+  font-size: ${th('fontSizeBaseSmall')};
+  
+  input {
+    position: absolute;
+    opacity: 0;
+    z-index: -1;
+  }
+
+  span::before {
+    content: ' ';
+    display: inline-block;
+    vertical-align: middle;
+
+    box-sizing: border-box;
+    width: ${th('space.3')};
+    height: ${th('space.3')};
+    margin-right: ${th('space.1')};
+
+    background: transparent;
+    border: 1px solid ${th('colorBorder')};
+    border-radius: ${th('borderRadius')};
+  }
+  
+  input:checked + span::before {
+    border-color: ${th('colorPrimary')};
+    background: url('data:image/svg+xml;utf8,${checkIcon}') center/75% no-repeat, ${th(
+  'colorPrimary',
+)};
+  }
+`
