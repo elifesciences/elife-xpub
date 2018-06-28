228e7b307ca46407c86aca11fa204e67b8c68ec3
Mihail Dunaev <mihail.dunaev91@gmail.com>
2018-06-28 16:11:19 +0100

    refactor(ui): remove the borderColor function from TextField
    re #220

--- client/elife-theme/src/elements/TextField.js
+++ client/elife-theme/src/elements/TextField.js
@@ -3,19 +3,6 @@ import { th } from '@pubsweet/ui-toolkit'
 
 /**
  * TODO
- * this should be removed once it's implemented in pubsweet
- * right now they don't have colorWarning
- */
-const borderColor = ({ theme, validationStatus = 'default' }) =>
-  ({
-    error: theme.colorError,
-    success: theme.colorSuccess,
-    warning: theme.colorWarning,
-    default: theme.colorBorder,
-  }[validationStatus])
-
-/**
- * TODO
  *
  * font-size & line-height might change to fontSizeBase in the future
  *
@@ -23,7 +10,6 @@ const borderColor = ({ theme, validationStatus = 'default' }) =>
  */
 export default {
   Input: css`
-    border-color: ${borderColor};
     color: ${th('colorText')};
     font-family: ${th('fontInterface')};
     font-size: 16px;
