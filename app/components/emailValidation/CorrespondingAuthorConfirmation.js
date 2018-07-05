3e09209b9b815579fab68040e2dfdae78f55550f
Jennifer Spencer <jennifer.spencer@yld.io>
2018-07-05 17:26:39 +0100

    refactor(verify corresponding author): remove log in button from Confirmation page

--- app/components/emailValidation/CorrespondingAuthorConfirmation.js
+++ app/components/emailValidation/CorrespondingAuthorConfirmation.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import { Flex, Box } from 'grid-styled'
 import { Heading } from '@pubsweet/ui'
-import LoginButton from '@pubsweet-pending/login-orcid/Login'
 
 const CorrespondingAuthorConfirmation = ({ location }) => (
   <Flex>
     <Box>
       <Heading level={1}>Thank you!</Heading>
-      <p>
-        You have been listed as the corresponding author for [title]. To track
-        the progress of this submission, please log in below.
-      </p>
-      <LoginButton location={location} />
+      <p>You have been listed as the corresponding author for [title].</p>
     </Box>
   </Flex>
 )
