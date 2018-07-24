9fd3ce300d0bebc1420a3df9c1080d6ff7e2d26f
Mihail Dunaev <mihail.dunaev91@gmail.com>
2018-07-24 17:08:03 +0100

    feat(submission): add react-select to search box
    re #347

--- app/components/ui/molecules/SearchBox.js
+++ app/components/ui/molecules/SearchBox.js
@@ -1,28 +1,116 @@
 import React from 'react'
-import { TextField } from '@pubsweet/ui'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
+import PropTypes from 'prop-types'
+import Select from 'react-select'
+import { th } from '@pubsweet/ui-toolkit'
 
 import Icon from '../atoms/Icon'
 
-const Root = styled.div`
-  position: relative;
-`
-
 const IconContainer = styled.div`
-  position: absolute;
-  right: 10px;
-  display: flex;
-  align-items: center;
-  height: 100%;
+  margin-right: ${th('space.2')};
 `
 
-const SearchBox = props => (
-  <Root>
-    <IconContainer>
-      <Icon size={3}>Search</Icon>
-    </IconContainer>
-    <TextField placeholder="Search..." {...props} />
-  </Root>
+const MyDropdownIndicator = ({ innerRef, innerProps }) => (
+  <IconContainer>
+    <Icon size={3} {...innerProps}>
+      Search
+    </Icon>
+  </IconContainer>
 )
 
-export default SearchBox
+class SearchBox extends React.Component {
+  state = {
+    selectedOption: null,
+    inputValue: '',
+  }
+  inputChange = inputValue => {
+    this.setState({ inputValue })
+  }
+  handleChange = selectedOption => {
+    this.setState({ selectedOption })
+  }
+  canSelectMore = () => {
+    const { selectedOption } = this.state
+    if (selectedOption) {
+      if (selectedOption.length > 1) {
+        return false
+      }
+    }
+    return true
+  }
+  isMenuOpen = () => {
+    const { inputValue } = this.state
+    if (this.canSelectMore()) {
+      return inputValue.length > 0
+    }
+    return false
+  }
+  isSearchable = () => this.canSelectMore()
+  render() {
+    const customStyles = {
+      control: (base, state) => ({
+        ...base,
+        marginBottom: '28px',
+      }),
+      valueContainer: (base, state) => ({
+        ...base,
+        padding: this.props.theme.space[2],
+        height: this.props.theme.space[5],
+      }),
+      multiValue: (base, state) => ({
+        ...base,
+        backgroundColor: this.props.theme.colorPrimary,
+        color: this.props.theme.colorTextReverse,
+        marginRight: '20px',
+        marginTop: '96px',
+        marginLeft: '-12px',
+      }),
+      multiValueLabel: (base, state) => ({
+        ...base,
+        color: this.props.theme.colorTextReverse,
+        fontSize: this.props.theme.fontSizeBase,
+        padding: '8px 4px 8px 8px',
+        paddingLeft: '8px',
+      }),
+      multiValueRemove: (base, state) => ({
+        ...base,
+        padding: `0 ${this.props.theme.gridUnit} 0 0`,
+        backgroundColor: this.props.theme.colorPrimary,
+        ':hover': {
+          cursor: 'pointer',
+          backgroundColor: this.props.theme.colorPrimary,
+          color: this.props.theme.colorTextReverse,
+        },
+      }),
+    }
+    return (
+      <Select
+        components={{
+          DropdownIndicator: MyDropdownIndicator,
+          IndicatorSeparator: null,
+          ClearIndicator: null,
+        }}
+        isMulti
+        isSearchable={this.isSearchable()}
+        menuIsOpen={this.isMenuOpen()}
+        onChange={this.handleChange}
+        onInputChange={this.inputChange}
+        options={this.props.options}
+        placeholder="Search..."
+        styles={customStyles}
+        value={this.state.selectedOption}
+      />
+    )
+  }
+}
+
+SearchBox.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+      label: PropTypes.string,
+    }),
+  ).isRequired,
+}
+
+export default withTheme(SearchBox)
--- app/components/ui/molecules/SearchBox.md
+++ app/components/ui/molecules/SearchBox.md
@@ -3,6 +3,10 @@ Search box
 ```js
 initialState = { value: '' }
 ;<SearchBox
+  options={[
+    { value: 'first', label: 'first option' },
+    { value: 'second', label: 'second option' },
+  ]}
   value={state.value}
   onChange={event => setState({ value: event.target.value })}
 />
