0af7f553fcda83c015c413ed1e350ceb646a438f
Mihail Dunaev <mihail.dunaev91@gmail.com>
2018-07-30 14:15:08 +0100

    feat(ui): replace material search icon with elife one
    re #347

---
+++ app/components/ui/atoms/icons/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default props => (
+  <svg viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+      <g
+        fill="#888888"
+        id="molecules/search-input"
+        transform="translate(-255.000000, -16.000000)"
+      >
+        <path d="M267.5,27 L266.7,27 L266.4,26.7 C267.4,25.6 268,24.1 268,22.5 C268,18.9 265.1,16 261.5,16 C257.9,16 255,18.9 255,22.5 C255,26.1 257.9,29 261.5,29 C263.1,29 264.6,28.4 265.7,27.4 L266,27.7 L266,28.5 L271,33.5 L272.5,32 L267.5,27 L267.5,27 Z M261.5,27 C259,27 257,25 257,22.5 C257,20 259,18 261.5,18 C264,18 266,20 266,22.5 C266,25 264,27 261.5,27 L261.5,27 Z" />
+      </g>
+    </g>
+  </svg>
+)
