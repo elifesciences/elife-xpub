820bf66bea379d6395acbb1a6da8b1ce37f967df
Jennifer Spencer <jennifer.spencer@yld.io>
2018-08-22 17:39:35 +0100

    feat(Disclosure): prevent submission without consent being ticked

--- app/components/pages/SubmissionWizard/steps/Disclosure/schema.js
+++ app/components/pages/SubmissionWizard/steps/Disclosure/schema.js
@@ -2,7 +2,10 @@ import * as yup from 'yup'
 
 const schema = yup.object().shape({
   submitterSignature: yup.string().required('Your name is required'),
-  disclosureConsent: yup.bool().required(),
+  disclosureConsent: yup
+    .bool()
+    .required()
+    .oneOf([true], 'Your consent is required for submission'),
 })
 
 export { schema }
