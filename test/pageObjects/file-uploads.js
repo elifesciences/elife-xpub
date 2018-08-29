1272ed2c8f7fbbcac69b9ee12a699f25e90584f2
Jennifer Spencer <jennifer.spencer@yld.io>
2018-08-29 16:25:04 +0100

    fix: bring urls in line with the names of our  wizard steps

--- test/pageObjects/file-uploads.js
+++ test/pageObjects/file-uploads.js
@@ -2,7 +2,7 @@ import config from 'config'
 import { Selector } from 'testcafe'
 
 const fileUploads = {
-  url: `${config.get('pubsweet-server.baseUrl')}/submit/upload`,
+  url: `${config.get('pubsweet-server.baseUrl')}/submit/files`,
   editor: Selector('[name="coverLetter"] div[contenteditable=true]'),
   manuscriptUpload: Selector('[data-test-id=upload]>input'),
   preview: Selector('[data-test-id=preview]'),
