fc02a24c766598f9e7798217a04ac7d953b69b13
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-09-04 16:16:16 +0100

    refactor: use knex query builder in file data access

--- server/xpub/entities/file/data-access.js
+++ server/xpub/entities/file/data-access.js
@@ -1,78 +1,58 @@
 const uuid = require('uuid')
-const lodash = require('lodash')
-const db = require('pubsweet-server/src/db')
+const { rowToEntity, entityToRow, buildQuery, runQuery } = require('../util')
 
-const mapRow = row =>
-  lodash.transform(row, (file, val, key) => {
-    const camelKey = key
-      .split('.')
-      .map(lodash.camelCase)
-      .join('.')
-    lodash.set(file, camelKey, val)
-  })
-
-const getValues = (id, file) => [
-  id,
-  file.manuscriptId,
-  file.filename,
-  file.label,
-  file.type,
-  file.mimeType,
-  file.size,
-  file.url,
+const columnNames = [
+  'manuscript_id',
+  'filename',
+  'label',
+  'type',
+  'mime_type',
+  'size',
+  'url',
 ]
 
 const dataAccess = {
   async selectById(id) {
-    const { rows } = await db.query(
-      `SELECT *
-         FROM file
-         WHERE id = $1`,
-      [id],
+    const { rows } = await runQuery(
+      buildQuery
+        .select()
+        .from('file')
+        .where({ id }),
     )
     if (!rows.length) {
       throw new Error('File not found')
     }
-    return mapRow(rows[0])
+    return rowToEntity(rows[0])
   },
 
   async selectAll() {
-    const { rows } = await db.query(`SELECT *
-                                     FROM file`)
-    return rows.map(mapRow)
+    const { rows } = await runQuery(buildQuery.select().from('file'))
+    return rows.map(rowToEntity)
   },
 
   async insert(file) {
-    const id = uuid.v4()
-    await db.query(
-      `INSERT INTO file(id, manuscript_id, filename, label, type, mime_type, size, url)
-         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-      getValues(id, file),
-    )
-    return id
+    const row = entityToRow(file, columnNames)
+    row.id = uuid.v4()
+    const query = buildQuery.insert(row).into('file')
+    await runQuery(query)
+    return row.id
   },
 
   update(file) {
-    return db.query(
-      `UPDATE file
-         SET manuscript_id = $2,
-             filename      = $3,
-             label         = $4,
-             type          = $5,
-             mime_type     = $6,
-             size          = $7,
-             url           = $8
-         WHERE id = $1`,
-      getValues(file.id, file),
-    )
+    const row = entityToRow(file, columnNames)
+    const query = buildQuery
+      .update(row)
+      .table('file')
+      .where('id', file.id)
+    return runQuery(query)
   },
 
   delete(id) {
-    return db.query(
-      `DELETE
-         FROM file
-         WHERE id = $1`,
-      [id],
+    return runQuery(
+      buildQuery
+        .delete()
+        .from('file')
+        .where({ id }),
     )
   },
 }
