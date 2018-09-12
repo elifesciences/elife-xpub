bffc97f50fcbcf93fc59f6d11c6106facb333d3f
Jennifer Spencer <jennifer.spencer@yld.io>
2018-09-12 11:21:42 +0100

    refactor: make SelectedItem accessible by using an HTML5 button element instead of making the icon itself clickable

--- app/components/ui/atoms/SelectedItem.js
+++ app/components/ui/atoms/SelectedItem.js
@@ -2,16 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 import { th } from '@pubsweet/ui-toolkit'
 
-import Icon from './Icon'
+import Icon from '../atoms/Icon'
 
-const CrossIcon = props => (
-  <Icon iconName="X" overrideName="@pubsweet-pending.Tags.Remove" {...props} />
+const CrossIcon = ({ overrideName, ...props }) => (
+  <Icon
+    iconName="Paperclip"
+    overrideName="@pubsweet-pending.Tags.Remove"
+    {...props}
+  />
 )
 
-const StyledIcon = styled(CrossIcon)`
-  margin-left: ${th('space.1')};
-  cursor: pointer;
+const IconButton = styled.button.attrs({
+  type: 'button',
+})`
+  background-color: transparent;
+  border: none;
+  line-height: 0;
+  padding: 0;
+`
+
+const RemoveButton = ({ onClick, ...props }) => (
+  <IconButton onClick={onClick} {...props}>
+    <CrossIcon />
+  </IconButton>
+)
+
+const StyledRemoveButton = styled(RemoveButton)`
   fill: ${th('colorTextReverse')};
+  margin-left: ${th('space.1')};
 `
 
 const Root = styled.div`
@@ -26,7 +44,7 @@ const Root = styled.div`
 
 const SelectedItem = ({ label, onCloseClick }) => (
   <Root>
-    {label} <StyledIcon onClick={onCloseClick} />
+    {label} <StyledRemoveButton onClick={onCloseClick} />
   </Root>
 )
 
