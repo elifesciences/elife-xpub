b48ba41a827b310a38599fd2e9cef5290eab8850
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-09-14 10:59:35 +0100

    test: fix e2e checkbox selectors to avoid waits

--- test/pageObjects/submission.js
+++ test/pageObjects/submission.js
@@ -8,13 +8,15 @@ const submission = {
   articleTypes: Selector('[role=option]'),
   subjectAreaLabel: Selector('label[for=subject-area-select]'),
 
-  discussionCheckbox: Selector('[name="previouslyDiscussedToggle"]'),
+  discussionCheckbox: Selector('[name="previouslyDiscussedToggle"]').parent(),
   discussionText: Selector('[name="previouslyDiscussed"]'),
 
-  previousArticleCheckbox: Selector('[name="previouslySubmittedToggle"]'),
+  previousArticleCheckbox: Selector(
+    '[name="previouslySubmittedToggle"]',
+  ).parent(),
   previousArticleText: Selector('[name="previouslySubmitted.0"]'),
 
-  cosubmissionCheckbox: Selector('[name="cosubmissionToggle"]'),
+  cosubmissionCheckbox: Selector('[name="cosubmissionToggle"]').parent(),
   firstCosubmissionTitle: Selector('[name="firstCosubmissionTitle"]'),
   secondCosubmissionTitle: Selector('[name="secondCosubmissionTitle"]'),
   moreSubmission: Selector('[name="moreSubmission"]'),
