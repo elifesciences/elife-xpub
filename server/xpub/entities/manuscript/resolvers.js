62eab86dac5b8f2bba6588cbbf1d90d1dded016c
Mihail Dunaev <mihail.dunaev91@gmail.com>
2018-09-20 17:52:51 +0100

    feat(graphql-subscriptions): wait for at least 500ms before sending upload progress
    re #546

--- server/xpub/entities/manuscript/resolvers.js
+++ server/xpub/entities/manuscript/resolvers.js
@@ -150,14 +150,24 @@ ${err}`,
 
       const pubsub = await getPubsub()
       let uploadedSize = 0
+      let startTime = process.hrtime()
       const progressReport = new Transform({
         transform(chunk, encoding, done) {
           uploadedSize += chunk.length
           const uploadProgress = Math.floor((uploadedSize * 100) / fileSize)
+          if (uploadProgress < 100) {
+            const elapsedTime = process.hrtime(startTime)
+            // MS_IN_S = 1e3, NS_IN_MS = 1e6
+            const elapsedTimeMs = elapsedTime[0] * 1e3 + elapsedTime[1] / 1e6
+            if (elapsedTimeMs < 500) {
+              return done(null, chunk)
+            }
+            startTime = process.hrtime()
+          }
           pubsub.publish(`${ON_UPLOAD_PROGRESS}.${user}`, {
             uploadProgress,
           })
-          done(null, chunk)
+          return done(null, chunk)
         },
       })
 
