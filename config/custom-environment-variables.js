4ec5bcf8de630dc2f69784ea29b60064a89a18b8
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2018-10-02 16:43:41 +0100

    feat: mapping database details to environment vars

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,5 +1,11 @@
 module.exports = {
   'pubsweet-server': {
+    db: {
+      user: 'PGUSER',
+      host: 'PGHOST',
+      database: 'PGDATABASE',
+      password: 'PGPASSWORD',
+    },
     hostname: 'PUBSWEET_HOSTNAME',
     baseUrl: 'PUBSWEET_BASEURL',
     secret: 'PUBSWEET_SECRET',
