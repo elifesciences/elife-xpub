542fcf2219f0925658861768c0bf7a8a05a67552
Mihail Dunaev <mihail.dunaev91@gmail.com>
2018-10-08 16:36:21 +0100

    fix(fonts): fix bad font path
    re #687

--- client/elife-theme/src/global.js
+++ client/elife-theme/src/global.js
@@ -1,4 +1,4 @@
-import { injectGlobal } from "styled-components";
+import { injectGlobal } from 'styled-components'
 
 injectGlobal`
   /* SemiBold */
@@ -6,7 +6,7 @@ injectGlobal`
     font-display: fallback;
     font-family: "Noto Sans SemiBold";
     src:
-      url('assets/fonts/NotoSans-SemiBold-webfont-custom-2-subsetting.woff2') format('woff2');
+      url('/assets/fonts/NotoSans-SemiBold-webfont-custom-2-subsetting.woff2') format('woff2');
   }
 
   /* Sans */
@@ -14,7 +14,7 @@ injectGlobal`
     font-display: fallback;
     font-family: "Noto Sans";
     src:
-      url('assets/fonts/NotoSans-Regular-webfont-custom-2-subsetting.woff2') format('woff2');
+      url('/assets/fonts/NotoSans-Regular-webfont-custom-2-subsetting.woff2') format('woff2');
   }
 
 
@@ -23,6 +23,6 @@ injectGlobal`
     font-display: fallback;
     font-family: "Noto Serif";
     src:
-      url('assets/fonts/NotoSerif-Regular-webfont-custom-2-subsetting.woff2') format('woff2');
+      url('/assets/fonts/NotoSerif-Regular-webfont-custom-2-subsetting.woff2') format('woff2');
   }
-`;
+`
