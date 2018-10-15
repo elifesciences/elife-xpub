dbe8c61f0f73d74a855cb334ff858fe5ef8a9935
jyenes <javier.yenes@yld.io>
2018-10-15 11:57:24 +0100

    style(DashboardListItem): style the component and add the date at the bottom of the relative date
    three parts of the component were styled, colors, size and date were added

--- app/components/ui/molecules/DashboardListItem.js
+++ app/components/ui/molecules/DashboardListItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styled, { withTheme } from 'styled-components'
 import { Flex, Box } from 'grid-styled'
-import { differenceInCalendarDays } from 'date-fns'
+import { differenceInCalendarDays, format } from 'date-fns'
 import { th } from '@pubsweet/ui-toolkit'
 
 const dashboardStatusColor = (status, theme) =>
   ({
-    1: theme.colorFurniture,
+    1: theme.colorTextSecondary,
     2: theme.colorError,
     3: theme.colorSuccess,
   }[status])
@@ -36,27 +36,37 @@ const Root = styled(Flex)`
   height: ${th('space.5')}
   align-items: center;
 `
-
 const StatusText = styled(Box)`
   color: ${props => dashboardStatusColor(props.status, props.theme)};
+  font-size: small;
 `
-
 const TitleText = styled(Box)`
   font-weight: bold;
+  font-size: small;
 `
-
 const DateText = styled(Box)`
-  color: ${props => props.theme.colorFurniture};
+  color: ${props => props.theme.colorTextSecondary};
+  text-align: right;
 `
-
+const RelativeDate = styled.p`
+  margin-bottom: 0;
+  font-size: small;
+`
+const SmallDate = styled.p`
+  font-size: x-small;
+  margin-top: 0;
+`
 const DashboardListItem = props => (
-    <Root {...props}>
-      <StatusText {...props} width={1 / 6}>
-        {dashboardStatusText(props.status)}
-      </StatusText>
-      <TitleText width={4 / 6}>{props.title}</TitleText>
-      <DateText width={1 / 6}>{dashboardDateText(props.date)}</DateText>
-    </Root>
-  )
+  <Root {...props}>
+    <StatusText {...props} width={1 / 6}>
+      {dashboardStatusText(props.status)}
+    </StatusText>
+    <TitleText width={4 / 6}>{props.title}</TitleText>
+    <DateText width={1 / 6}>
+      <RelativeDate>{dashboardDateText(props.date)}</RelativeDate>
+      <SmallDate>{format(props.date, 'ddd D MMM YYYY')}</SmallDate>
+    </DateText>
+  </Root>
+)
 
 export default withTheme(DashboardListItem)
--- app/components/ui/molecules/DashboardListItem.md
+++ app/components/ui/molecules/DashboardListItem.md
@@ -2,8 +2,10 @@ TODO
 
 ```js
 ;<DashboardListItem
-  status={2}
-  title={'This is the title'}
+  status={1}
+  title={
+    'Shearing in flow environment promotes evolution of social behaviour in microbial populations'
+  }
   date={'2018-10-09T16:10:00.000Z'}
 />
 ```
