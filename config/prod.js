46960f518a4645a7402ae3ad87c7fdfd4e7a7cdd
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2018-10-18 15:56:40 +0100

    feat: enabling production login and api

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,15 @@ module.exports = {
   'pubsweet-server': {
     logger,
   },
+  elife: {
+    api: {
+      url: 'https://api.elifesciences.org/',
+    },
+  },
+  login: {
+    url: 'https://elifesciences.org/submit',
+    enableMock: false,
+  },
   meca: {
     sftp: {
       disableUpload: true,
