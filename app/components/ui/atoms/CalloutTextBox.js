2501ca2c47530ba7d18a4e5cfc1cb8dcb9791d44
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-10-18 16:59:55 +0100

    feat: callout text box

---
+++ app/components/ui/atoms/CalloutTextBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Box } from 'grid-styled'
+import { th } from '@pubsweet/ui-toolkit'
+
+const Frame = styled(Box)`
+  border: ${th('borderWidth')} ${th('borderStyle')} ${th('colorPrimary')};
+  background-color: ${th('colorPrimary')}20;
+`
+
+const CalloutTextBox = ({ onClose, children, ...props }) => (
+  <Frame mb={2} p={3} {...props}>
+    {children}{' '}
+  </Frame>
+)
+export default CalloutTextBox
---
+++ app/components/ui/atoms/CalloutTextBox.md
@@ -0,0 +1,8 @@
+```js
+<CalloutTextBox>
+  On January 1, 2017, eLife introduced a fee for publication. A fee of $2,500*
+  is collected for all published papers submitted on or after this date;
+  however, authors with insufficient funding to pay the fee are eligible for a
+  fee waiver (read more).
+</CalloutTextBox>
+```
