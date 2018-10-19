ada469d99800df63567e8debcb4dcddb14176a0d
Jennifer Spencer <jennifer.spencer@yld.io>
2018-10-19 18:32:56 +0100

    refactor(List): store shared styling in separate variable then use in both types of list

--- app/components/ui/atoms/List.js
+++ app/components/ui/atoms/List.js
@@ -1,8 +1,9 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { th } from '@pubsweet/ui-toolkit'
 import { List } from 'react-feather'
 
-const StyledUnorderedList = styled.ul`
+const sharedListStyling = css`
+  /* remove default margin & padding */
   margin-left: 0;
   padding-left: 0;
 
@@ -39,50 +40,22 @@ const StyledUnorderedList = styled.ul`
     margin-bottom: 0;
   }
 `
+const StyledUnorderedList = styled.ul`
+  ${props => sharedListStyling};
+`
 
 const StyledOrderedList = styled.ol`
-  /* ol itself - initiate a counter & remove default margin & padding */
-  counter-reset: li;
-  margin-left: 0;
-  padding-left: 0;
+  ${props => sharedListStyling};
 
-  /* Numbering & box around number */
+  counter-reset: li; /* initiate a counter */
 
   > li:before {
     content: counter(li) '.'; /* Use the counter + a full stop as the content */
     counter-increment: li; /* Increment the counter by 1 */
-
-    /* Position and style the box around the number/bullet */
-    position: absolute;
-    left: -${th('space.3')};
-    -moz-box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    width: ${th('space.3')};
-
-    /* font of number or bullet */
-    color: ${th('colorText')};
-    font-family: inherit;
-    font-size: inherit;
-    text-align: center;
   }
 
   > li {
     list-style: none; /* Disable the normal item numbering */
-    position: relative;
-
-    /* Spacing between list items */
-    margin-bottom: ${th('space.3')};
-
-    /* Give each list item a left margin to make room for the numbers */
-    margin-left: ${th('space.3')};
-
-    /* Moves text slightly further away from the number, bringing the total space closer to 12px  */
-    padding-left: ${th('space.1')};
-  }
-
-  > li:last-child {
-    margin-bottom: 0;
   }
 `
 
