2ba0901347924ed313c5159abc4dcd2b33666681
Cory Donkin <corydanglia@gmail.com>
2018-10-19 08:17:06 +0100

    re-name test selector to status to fix e2e test

--- test/pageObjects/dashboard.js
+++ test/pageObjects/dashboard.js
@@ -5,7 +5,7 @@ const dashboard = {
   url: `${config.get('pubsweet-server.baseUrl')}`,
   submitManuscript: Selector('[data-test-id=submit]'),
   titles: Selector('[data-test-id=title]'),
-  stages: Selector('[data-test-id=stage]'),
+  statuses: Selector('[data-test-id=status]'),
 }
 
 export default dashboard
