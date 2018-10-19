c99a5d4d87204aa88ffa226eaaca8255d518d333
Jennifer Spencer <jennifer.spencer@yld.io>
2018-10-19 17:36:52 +0100

    feat(UnorderedList): create unordered list based on ordered list & refactor to make one 'List' component

--- app/components/ui/atoms/List.js
+++ app/components/ui/atoms/List.js
@@ -1,5 +1,44 @@
 import styled from 'styled-components'
 import { th } from '@pubsweet/ui-toolkit'
+import { List } from 'react-feather'
+
+const StyledUnorderedList = styled.ul`
+  margin-left: 0;
+  padding-left: 0;
+
+  > li:before {
+    /* Position and style the box around the bullet */
+    position: absolute;
+    left: -${th('space.3')};
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    width: ${th('space.3')};
+
+    /* font of number or bullet */
+    color: ${th('colorText')};
+    font-family: inherit;
+    font-size: inherit;
+    text-align: center;
+  }
+
+  > li {
+    position: relative;
+
+    /* Spacing between list items */
+    margin-bottom: ${th('space.3')};
+
+    /* Give each list item a left margin to make room for the bullets */
+    margin-left: ${th('space.3')};
+
+    /* Moves text slightly further away from the number, bringing the total space closer to 12px  */
+    padding-left: ${th('space.1')};
+  }
+
+  > li:last-child {
+    margin-bottom: 0;
+  }
+`
 
 const StyledOrderedList = styled.ol`
   /* ol itself - initiate a counter & remove default margin & padding */
@@ -47,4 +86,7 @@ const StyledOrderedList = styled.ol`
   }
 `
 
-export default StyledOrderedList
+List.Ordered = StyledOrderedList
+List.Unordered = StyledUnorderedList
+
+export default List
