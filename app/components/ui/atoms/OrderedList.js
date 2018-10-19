39486225a784bf3afb4697ee9898f664fa96c4ba
Jennifer Spencer <jennifer.spencer@yld.io>
2018-10-19 17:06:58 +0100

    feat(OrderedList): create eLife-styled <ol> using this as a reference https://www.456bereastreet.com/archive/201105/styling_ordered_list_numbers/

---
+++ app/components/ui/atoms/OrderedList.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+import { th } from '@pubsweet/ui-toolkit'
+
+const StyledOrderedList = styled.ol`
+  /* ol itself - initiate a counter & remove default margin & padding */
+  counter-reset: li;
+  margin-left: 0;
+  padding-left: 0;
+
+  /* Numbering & box around number */
+
+  > li:before {
+    content: counter(li) '.'; /* Use the counter + a full stop as the content */
+    counter-increment: li; /* Increment the counter by 1 */
+
+    /* Position and style the box around the number/bullet */
+    position: absolute;
+    left: -${th('space.3')};
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    width: ${th('space.3')};
+
+    /* font of number or bullet */
+    color: ${th('colorText')};
+    font-family: inherit;
+    font-size: inherit;
+    text-align: center;
+  }
+
+  > li {
+    list-style: none; /* Disable the normal item numbering */
+    position: relative;
+
+    /* Spacing between list items */
+    margin-bottom: ${th('space.3')};
+
+    /* Give each list item a left margin to make room for the numbers */
+    margin-left: ${th('space.3')};
+
+    /* Moves text slightly further away from the number, bringing the total space closer to 12px  */
+    padding-left: ${th('space.1')};
+  }
+
+  > li:last-child {
+    margin-bottom: 0;
+  }
+`
+
+export default StyledOrderedList
---
+++ app/components/ui/atoms/OrderedList.md
@@ -0,0 +1,28 @@
+```js
+<OrderedList>
+  <li>
+    Title: The title should ideally be fewer than 120 characters, with a clear
+    indication of the biological system under investigation (if appropriate),
+    and should avoid abbreviations and unfamiliar acronyms if possible. Please
+    note that two-part titles – e.g. “What goes up must come down: Oscillations
+    in transcriptional networks” – are not permitted for research papers.
+  </li>
+  <li>
+    Author names and affiliations: A complete list of authors and affiliations
+    should be provided in the paper itself.
+  </li>
+  <li>
+    Abstract: The abstract should be fewer than 150 words and should not contain
+    subheadings. It should provide a clear, measured, and concise summary of the
+    work. If the biological system (species names or broader taxonomic groups if
+    appropriate) is not mentioned in the title, it must be included in the
+    abstract.
+  </li>
+  <li>
+    Introduction, Results, Discussion, Materials and Methods: We encourage a
+    clear and concise style of writing. Various writing guides are available,
+    including Preparing a Manuscript for Submission to a Biomedical Journal
+    (ICMJE) and The Elements of Style (New York: bartleby.com, 1999).
+  </li>
+</OrderedList>
+```
