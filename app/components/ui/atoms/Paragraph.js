ef117446a677b360bb2b27553c2cfee100ad8406
Jennifer Spencer <jennifer.spencer@yld.io>
2018-10-22 17:42:36 +0100

    style(Paragraphs): add bottom margin to Paragraph & SmallParagraph except when it is the final paragraph on the page / in that section

--- app/components/ui/atoms/Paragraph.js
+++ app/components/ui/atoms/Paragraph.js
@@ -5,6 +5,13 @@ const Paragraph = styled.p`
   font-family: ${th('fontReading')};
   font-size: ${th('fontSizeBase')};
   line-height: ${th(`lineHeightBase`)};
+
+  margin-top: 0;
+  margin-bottom: ${th('space.3')};
+
+  &:last-child {
+    margin-bottom: 0;
+  }
 `
 
 export default Paragraph
