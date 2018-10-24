c80e43ac2afadf47b992ba09f08910aee094ab55
jyenes <javier.yenes@yld.io>
2018-10-24 16:10:36 +0100

    style(StaffGrid): refactor the code

--- app/components/ui/molecules/StaffGrid.js
+++ app/components/ui/molecules/StaffGrid.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Flex, Box } from 'grid-styled'
-import { th } from '@pubsweet/ui-toolkit'
-
-const Card = styled(Box)`
-  padding-top: ${th('space.3')};
-`
 
 const StaffGrid = ({ children, ...props }) => (
-  <Flex flexWrap="wrap" mx={-2} {...props}>
+  <Flex flexWrap="wrap" mb={-3} mx={-2} {...props}>
     {children.map((item, index) => (
-      <Card key={item.key || index} px={2} width={[1 / 2, 1 / 2, 1 / 3]}>
+      <Box key={item.key || index} mb={3} px={2} width={[1 / 2, 1 / 2, 1 / 3]}>
         {item}
-      </Card>
+      </Box>
     ))}
   </Flex>
 )
