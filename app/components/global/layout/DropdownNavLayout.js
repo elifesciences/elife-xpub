667909b066115b35d3e76ddae1b44dba0accdd49
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2018-10-24 12:09:09 +0100

    feat: using react-select for dropdown

--- app/components/global/layout/DropdownNavLayout.js
+++ app/components/global/layout/DropdownNavLayout.js
@@ -3,27 +3,50 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { th } from '@pubsweet/ui-toolkit'
 import NavLink from '../../ui/atoms/NavLink'
+import Select, { createFilter, components } from 'react-select'
+
+const Root = styled.div``
 
 const DropdownNavLink = styled(NavLink)`
   display: block;
   padding: ${th('space.1')};
 `
 
-const DropdownNavLayout = ({ children, navList }) => (
-  <React.Fragment>
-    {navList &&
-      navList.map(navItem => (
-        <DropdownNavLink
-          data-test-id="side-nav-link"
-          key={navItem.link}
-          to={navItem.link}
-        >
-          {navItem.label}
-        </DropdownNavLink>
-      ))}
-    {children}
-  </React.Fragment>
-)
+class DropdownNavLayout extends React.Component {
+
+  constructor(props) {
+    super(props)
+    const navItems = this.props.navList.map(navItem =>
+      navItem.label
+    )
+    this.state = {
+      navItems,
+      selected: navItems[0]
+    }
+  }
+
+  handleChange(selected) {
+    this.setState({ selected });
+  }
+
+  render() {
+    const { navItems, selected } = this.state
+    const selectProps = {
+      value: selected,
+      options: navItems,
+      onChange: this.handleChange
+    }
+    return (
+      <Root>
+        <Select
+          {...selectProps}
+        />
+        {this.props.children}
+      </Root>
+    )
+  }
+
+}
 
 DropdownNavLayout.propTypes = {
   navList: PropTypes.arrayOf(
