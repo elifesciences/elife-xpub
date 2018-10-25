db5a77a9ea4ad4072a07609a3c85242efeafcc44
jyenes <javier.yenes@yld.io>
2018-10-25 11:04:49 +0100

    feat(Staff Pages): creating Editorial and Production Staff Pages

---
+++ app/components/pages/Staff/EditorialPage.md
@@ -0,0 +1,5 @@
+Static Staff Page.
+
+```js
+<EditorialPage />
+```
--- app/components/ui/molecules/StaffCard.js
+++ app/components/ui/molecules/StaffCard.js
@@ -2,16 +2,12 @@ import React from 'react'
 import PropType from 'prop-types'
 import styled from 'styled-components'
 import { th } from '@pubsweet/ui-toolkit'
-import media from '../../global/layout/media'
 
 const StaffImg = styled.img`
   
   border: ${th('borderWidth')} ${th('borderStyle')} ${th('colorFurniture')}
   margin-bottom: ${th('space.1')};
   max-width: 100%;
-  ${media.mobileUp`
-    height: 280px;
-  `}
 `
 
 const StaffName = styled.div`
