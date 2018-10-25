ae901635f7b6ce3381c4306ba49afdef6a991ab1
Tamlyn Rhodes <tamlyn@tamlyn.org>
2018-10-25 13:59:51 +0100

    fix: tidy up
    Remove some unused code
    Reinstate the coverage config (but leave it off by default)
    Add some environment variables to config

--- webpack/babel-includes.js
+++ webpack/babel-includes.js
@@ -12,11 +12,10 @@ module.exports = [
 
   // include pubsweet and xpub packages which are published untranspiled
   /xpub-[^/]+\/src/,
-  /wax-[^/]+\/src/,
   /pubsweet-[^/\\]+\/(?!node_modules)/,
   /@pubsweet\/[^/\\]+\/(?!node_modules)/,
 
-  // include pubsweet packages when npm linked from monorepo
+  // include pubsweet packages when npm linked from pubsweet monorepo
   filepath =>
     // is a child of packages but not node_modules
     filepath.match(/\/packages\//) && !filepath.match(/\/node_modules\//),
