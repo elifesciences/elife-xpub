4f2316b730ea27ca6e1e9b71fa12c4915f87dcb8
Jennifer Spencer <jennifer.spencer@yld.io>
2018-10-26 12:14:44 +0100

    refactor: add error boundary around static page routing

--- app/components/pages/StaticPages/index.js
+++ app/components/pages/StaticPages/index.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import { Box, Flex } from 'grid-styled'
 import { th } from '@pubsweet/ui-toolkit'
 
+import ErrorBoundary from '../../global/ErrorBoundary'
 import media from '../../global/layout/media'
 import NavLink from '../../ui/atoms/NavLink'
 
@@ -28,44 +29,46 @@ const SideNavLink = styled(NavLink)`
 
 const StaticPage = ({ navList }) => (
   <div>
-    <TopNavContainer>{/* TODO: dropdown goes here */}</TopNavContainer>
+    <ErrorBoundary>
+      <TopNavContainer>{/* TODO: dropdown goes here */}</TopNavContainer>
 
-    <Flex>
-      <SideNavContainer
-        ml={[0, 0, '8.33%']} // 1 column's worth of spacing (in a 12 column grid)
-        pr={3}
-        pt={18} // To match the top padding on the H1 in the 'main' component
-        width={[0, 3 / 12, 3 / 12, 2 / 12]}
-      >
-        <Box is="nav">
-          {navList &&
-            navList.map(navItem => (
-              <SideNavLink
-                data-test-id="side-nav-link"
-                key={navItem.link}
-                to={navItem.link}
-              >
-                {navItem.label}
-              </SideNavLink>
-            ))}
-        </Box>
-      </SideNavContainer>
+      <Flex>
+        <SideNavContainer
+          ml={[0, 0, '8.33%']} // 1 column's worth of spacing (in a 12 column grid)
+          pr={3}
+          pt={18} // To match the top padding on the H1 in the 'main' component
+          width={[0, 3 / 12, 3 / 12, 2 / 12]}
+        >
+          <Box is="nav">
+            {navList &&
+              navList.map(navItem => (
+                <SideNavLink
+                  data-test-id="side-nav-link"
+                  key={navItem.link}
+                  to={navItem.link}
+                >
+                  {navItem.label}
+                </SideNavLink>
+              ))}
+          </Box>
+        </SideNavContainer>
 
-      <MainContainer width={[1, 1, 7 / 12, 7 / 12, 8 / 12]}>
-        <Switch>
-          {navList &&
-            navList.map(navItem => (
-              <Route
-                component={navItem.component}
-                exact
-                key={navItem.link}
-                path={navItem.link}
-              />
-            ))}
-          <Redirect to={navList[0].link} />
-        </Switch>
-      </MainContainer>
-    </Flex>
+        <MainContainer width={[1, 1, 7 / 12, 7 / 12, 8 / 12]}>
+          <Switch>
+            {navList &&
+              navList.map(navItem => (
+                <Route
+                  component={navItem.component}
+                  exact
+                  key={navItem.link}
+                  path={navItem.link}
+                />
+              ))}
+            <Redirect to={navList[0].link} />
+          </Switch>
+        </MainContainer>
+      </Flex>
+    </ErrorBoundary>
   </div>
 )
 
