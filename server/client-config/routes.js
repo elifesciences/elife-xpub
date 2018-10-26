d9a89e89b518a926a9bf17875b3263987a690b44
jyenes <javier.yenes@yld.io>
2018-10-26 18:26:35 +0200

    feat: check if database exists

--- server/client-config/routes.js
+++ server/client-config/routes.js
@@ -1,9 +1,16 @@
 const config = require('config')
 const { pickBy } = require('lodash')
+const DBExists = require('@pubsweet/db-manager/src/helpers/db-exists.js')
 
 const template = clientConfig => `
   window.config = ${JSON.stringify(clientConfig)}
 `
+const nocache = (req, res, next) => {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+  res.header('Expires', '-1')
+  res.header('Pragma', 'no-cache')
+  next()
+}
 
 module.exports = app => {
   app.get('/config.js', (req, res) => {
@@ -13,7 +20,17 @@ module.exports = app => {
     res.send(response)
   })
 
-  app.get('/ping', (req, res) => {
-    res.send('pong')
+  app.get('/ping', nocache, async (req, res) => {
+    try {
+      const exists = await DBExists()
+      if (!exists) {
+        res.send(404, 'database error')
+      }
+      // TODO: cretate a function to check S3 connection
+      // let responseS3 = await checkS3();
+      res.send('pong')
+    } catch (error) {
+      res.send(500, 'ko')
+    }
   })
 }
