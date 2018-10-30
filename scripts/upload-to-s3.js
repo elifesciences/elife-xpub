e9510ceac5a2828f7d7b0481438caac7b9b2ce65
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2018-10-30 17:41:57 +0000

    feat: uploading files to S3

--- scripts/upload-to-s3.js
+++ scripts/upload-to-s3.js
@@ -1,14 +1,35 @@
+/*
+ * Uploads a folder from your local volume to AWS S3
+ *
+ * node scripts/upload-to-s3.js FROM_DIR TO_DIR
+ *
+ * - FROM_DIR: local folder to upload. Default=build
+ * - TO_DIR: folder within S3 to upload to.
+ *   Will be created if the folder doesn't exist .
+ *   Default=build
+ */
+
+
 global.__basedir = `${__dirname}/../`
 
 const AWS = require('aws-sdk')
 const config = require('config')
 const fs = require('fs')
 const path = require('path')
+const _ = require('lodash')
+const mime = require('mime')
 const { promisify } = require('util')
 
 const readdir = promisify(fs.readdir)
 const stat = promisify(fs.stat)
-const dirPath = path.join(__basedir, '_build_styleguide/')
+const readFile = promisify(fs.readFile)
+
+const [
+  fromDir='build',
+  toDir='build'
+] = process.argv.slice(2,4)
+
+const dirPath = path.join(__basedir, fromDir)
 
 const S3 = new AWS.S3({
   ...config.get('aws.credentials'),
@@ -16,30 +37,42 @@ const S3 = new AWS.S3({
   apiVersion: '2006-03-01',
 })
 
-async function read(dir) {
+async function getFiles(dir, { prefix='' }={}) {
   const files = await readdir(dir)
-  return await Promise.all(files.map(
+  const filePaths = await Promise.all(files.map(
     async (fileName) => {
       const filePath = path.join(dir, fileName)
       const pathStat = await stat(filePath)
       if(pathStat.isFile())
-        return filePath
+        return { fileName: `${prefix}${fileName}`, filePath }
       else if (pathStat.isDirectory())
-        return await read(filePath)
+        return await getFiles(filePath, { prefix: `${prefix}${fileName}/` })
     }
   ))
+  return _.flattenDeep(filePaths)
 }
 
 async function upload() {
-  const files = await read(dirPath)
+  const files = await getFiles(dirPath)
+  if(!files || files.length === 0) {
+    throw new Error(`Directory '${dirPath}' is empty or does not exist.`)
+  }
   await uploadToS3(files)
 }
 
 async function uploadToS3(files){
-  if(!files || files.length === 0) {
-    throw new Error(`Directory '${dirPath}' is empty or does not exist.`)
-  }
-  // await S3.putObject(params).promise()
+  await Promise.all(files.map(
+    async ({ fileName, filePath }) => {
+      const Body = await readFile(filePath)
+      const params = {
+        Body,
+        Key: `${toDir}/${fileName}`,
+        ContentType: mime.getType(path.extname(fileName)),
+      }
+      console.log(`Uploading ${params.Key}`)
+      await S3.putObject(params).promise()
+    }
+  ))
 }
 
 upload()
