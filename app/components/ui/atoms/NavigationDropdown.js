f92875e7185e054fc68cf5d30ffd7ce390b0c7f9
Jennifer Spencer <jennifer.spencer@yld.io>
2018-10-30 14:06:00 +0000

    feat(NavigationDropdown): enable selected item to be passed in from parent remove selected item from list

--- app/components/ui/atoms/NavigationDropdown.js
+++ app/components/ui/atoms/NavigationDropdown.js
@@ -2,9 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Select from 'react-select'
 
-const NavigationDropdown = ({ options, ...props }) => (
-  <Select options={options} />
-)
+class NavigationDropdown extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: this.props.defaultValue,
+    }
+  }
+
+  render() {
+    const { options } = this.props
+
+    return (
+      <Select
+        getOptionLabel={({ label }) => label}
+        getOptionValue={({ value }) => value}
+        onChange={({ value }) => this.setState({ value })}
+        options={options.filter(({ value }) => value !== this.state.value)}
+        placeholder=""
+        value={options.filter(({ value }) => value === this.state.value)}
+      />
+    )
+  }
+}
 
 NavigationDropdown.propTypes = {
   options: PropTypes.arrayOf(
@@ -13,6 +34,7 @@ NavigationDropdown.propTypes = {
       value: PropTypes.string.isRequired,
     }),
   ).isRequired,
+  defaultValue: PropTypes.string.isRequired,
 }
 
 export default NavigationDropdown
--- app/components/ui/atoms/NavigationDropdown.md
+++ app/components/ui/atoms/NavigationDropdown.md
@@ -1,9 +1,8 @@
 ```js
-;<NavigationDropdown
-  options={[
-    { label: 'Contact eLife', value: '/contact-us' },
-    { label: 'Editorial staff', value: '/contact-us/editorial-staff' },
-    { label: 'Production staff', value: '/contact-us/production-staff' },
-  ]}
-/>
+const options = [
+  { label: 'Contact eLife', value: '/contact-us' },
+  { label: 'Editorial staff', value: '/contact-us/editorial-staff' },
+  { label: 'Production staff', value: '/contact-us/production-staff' },
+]
+;<NavigationDropdown options={options} defaultValue={options[2].value} />
 ```
