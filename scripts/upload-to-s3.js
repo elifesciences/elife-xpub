f5033c70818e2e1519e99a850c2de32b2b58aa4a
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2018-10-30 14:48:01 +0000

    feat: reading files from styleguide build

--- scripts/upload-to-s3.js
+++ scripts/upload-to-s3.js
@@ -1,5 +1,13 @@
+global.__basedir = `${__dirname}/../`
+
 const AWS = require('aws-sdk')
 const config = require('config')
+const fs = require('fs')
+const path = require('path')
+const { promisify } = require('util')
+
+const readdir = promisify(fs.readdir)
+const dirPath = path.join(__basedir, '_build_styleguide/')
 
 const S3 = new AWS.S3({
   ...config.get('aws.credentials'),
@@ -7,10 +15,15 @@ const S3 = new AWS.S3({
   apiVersion: '2006-03-01',
 })
 
-// TODO
-const params = {};
+async function upload() {
+  const files = await readdir(dirPath)
+  if(!files || files.length === 0) {
+    throw new Error(`Directory '${dirPath}' is empty or does not exist.`)
+  }
+  // await S3.putObject(params).promise()
+}
 
-S3.putObject(params).promise()
+upload()
   .then(() => {
     console.log('Successfully uploaded to S3.')
     process.exit(0)
