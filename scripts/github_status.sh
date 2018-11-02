cd47684b8d19f6902808da04fed2fdaf33c0ef78
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2018-11-02 17:23:15 +0000

    feat: added script to update PR statuses

---
+++ scripts/github_status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+status=$1
+context=$2
+targetUrl=$3
+description=$4
+version=${CI_COMMIT_SHA};
+apiUrl=https://api.github.com/repos/elifesciences/elife-xpub/statuses/${version}
+postData='{"state": "'$status'", "target_url": "'${targetUrl}'", "context": "'${context}'", "description": "'${description}'"}'
+curl -u gitlab-ci:$github_token_write $apiUrl -d "${postData}"
