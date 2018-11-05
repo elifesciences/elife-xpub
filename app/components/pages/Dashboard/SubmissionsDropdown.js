02b8d229c434ed1e2da8ab31bbf050816b91ee61
Jennifer Spencer <jennifer.spencer@yld.io>
2018-11-05 15:05:56 +0000

    feat(SubmissionsDropdown): create component that combines dropdown & content

---
+++ app/components/pages/Dashboard/SubmissionsDropdown.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Box } from 'grid-styled'
+import { th } from '@pubsweet/ui-toolkit'
+
+import media from '../../global/layout/media'
+import Submissions from './panels/Submissions'
+import Archived from './panels/Archived'
+import NavigationDropdown from '../../ui/atoms/NavigationDropdown'
+
+const MobileOnlySubmissionsContainer = styled(Box).attrs({ mx: -3 })`
+  border-bottom: ${th('borderWidth')} ${th('borderStyle')} ${th('colorBorder')};
+  margin-bottom: ${th('space.3')};
+  ${media.mobileUp`display: none;`};
+`
+
+class SubmissionsDropdown extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      submissionState: 'submissions',
+    }
+  }
+
+  handleSelection = selectedItem => {
+    this.setState({ submissionState: selectedItem.value })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <MobileOnlySubmissionsContainer>
+          <NavigationDropdown
+            onSelection={this.handleSelection}
+            options={[
+              {
+                label: 'Submissions',
+                value: 'submissions',
+              },
+              { label: 'Archive', value: 'archive' },
+            ]}
+            value={this.state.submissionState}
+          />
+        </MobileOnlySubmissionsContainer>
+        {this.state.submissionState === 'submissions' && <Submissions />}
+        {this.state.submissionState === 'archive' && <Archived />}
+      </React.Fragment>
+    )
+  }
+}
+
+export default SubmissionsDropdown
