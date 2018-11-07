2f8b8bb79fa97ec17b779c0bc2d8a3a7088c8775
Cory <c.donkin@elifesciences.org>
2018-11-07 10:29:10 +0000

    Add woff fallback for fonts

--- client/elife-theme/src/global.js
+++ client/elife-theme/src/global.js
@@ -6,7 +6,8 @@ injectGlobal`
     font-display: fallback;
     font-family: "Noto Sans SemiBold";
     src:
-      url('/assets/fonts/NotoSans-SemiBold-webfont-custom-2-subsetting.woff2') format('woff2');
+      url('/assets/fonts/NotoSans-SemiBold-webfont-custom-2-subsetting.woff2') format('woff2'),
+      url('/assets/fonts/NotoSans-SemiBold-webfont-custom-2-subsetting.woff') format('woff');
   }
 
   /* Sans */
@@ -14,7 +15,8 @@ injectGlobal`
     font-display: fallback;
     font-family: "Noto Sans";
     src:
-      url('/assets/fonts/NotoSans-Regular-webfont-custom-2-subsetting.woff2') format('woff2');
+      url('/assets/fonts/NotoSans-Regular-webfont-custom-2-subsetting.woff2') format('woff2'),
+      url('/assets/fonts/NotoSans-Regular-webfont-custom-2-subsetting.woff') format('woff');
   }
 
 
@@ -23,6 +25,7 @@ injectGlobal`
     font-display: fallback;
     font-family: "Noto Serif";
     src:
-      url('/assets/fonts/NotoSerif-Regular-webfont-custom-2-subsetting.woff2') format('woff2');
+      url('/assets/fonts/NotoSerif-Regular-webfont-custom-2-subsetting.woff2') format('woff2'),
+      url('/assets/fonts/NotoSerif-Bold-webfont-basic-latin-subsetting.woff') format('woff');
   }
 `
