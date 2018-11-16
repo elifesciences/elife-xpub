c62e630c35edbd3ed3384fc32a738a8432ebe59d
Cory <c.donkin@elifesciences.org>
2018-11-16 08:10:36 +0000

    Add mutation for deleting manuscript file and call on client error

--- app/components/pages/SubmissionWizard/steps/Files/index.js
+++ app/components/pages/SubmissionWizard/steps/Files/index.js
@@ -22,6 +22,18 @@ const UPLOAD_MUTATION = gql`
   }
 `
 
+const DELETE_MUTATION = gql`
+  mutation UploadFile($id: ID!) {
+    removeUploadedManuscript(id: $id) {
+      id
+      files {
+        filename
+        type
+      }
+    }
+  }
+`
+
 function getProgress(loading, data) {
   return loading || !data.uploadProgress ? 0 : data.uploadProgress
 }
@@ -40,52 +52,75 @@ const FilesPageContainer = ({
     {(uploadFile, { loading, error: uploadError }) => {
       const fieldName = 'files'
       return (
-        <React.Fragment>
-          <Box mb={3} width={1}>
-            <ValidatedField
-              component={CoverLetterEditor}
-              id="coverLetter"
-              name="coverLetter"
-              onBlur={value => setFieldValue('coverLetter', value)}
-              onChange={value => setFieldValue('coverLetter', value)}
-            />
-          </Box>
-          <Box width={1}>
-            <ManuscriptUpload
-              conversion={{
-                converting: loading || uploadData.uploadProgress < 100,
-                // TODO import this constant from somewhere (data model package?)
-                completed: values[fieldName].some(
-                  file => file.type === 'MANUSCRIPT_SOURCE',
-                ),
-                progress: getProgress(uploadLoading, uploadData),
-                error: uploadError,
-              }}
-              data-test-id="upload"
-              formError={touched[fieldName] && errors[fieldName]}
-              onDrop={files => {
-                setFieldTouched(fieldName, true, false)
+        <Mutation mutation={DELETE_MUTATION}>
+          {deleteFile => (
+            <React.Fragment>
+              <Box mb={3} width={1}>
+                <ValidatedField
+                  component={CoverLetterEditor}
+                  id="coverLetter"
+                  name="coverLetter"
+                  onBlur={value => setFieldValue('coverLetter', value)}
+                  onChange={value => setFieldValue('coverLetter', value)}
+                />
+              </Box>
+              <Box width={1}>
+                <ManuscriptUpload
+                  conversion={{
+                    converting: loading || uploadData.uploadProgress < 100,
+                    // TODO import this constant from somewhere (data model package?)
+                    completed: values[fieldName].some(
+                      file => file.type === 'MANUSCRIPT_SOURCE',
+                    ),
+                    progress: getProgress(uploadLoading, uploadData),
+                    error: uploadError,
+                  }}
+                  data-test-id="upload"
+                  formError={touched[fieldName] && errors[fieldName]}
+                  onDrop={files => {
+                    setFieldTouched(fieldName, true, false)
 
-                if (files.length > 1) {
-                  setFieldError(fieldName, errorMessageMapping.MULTIPLE)
-                  return
-                }
-                if (files.length === 0) {
-                  setFieldError(fieldName, errorMessageMapping.UNSUPPORTED)
-                  return
-                }
+                    if (files.length > 1) {
+                      setFieldError(fieldName, errorMessageMapping.MULTIPLE)
+                      deleteFile({
+                        variables: { id: values.id },
+                      }).then(({ data }) => {
+                        setFieldValue(
+                          fieldName,
+                          data.removeUploadedManuscript.files,
+                        )
+                      })
+                      return
+                    }
+                    if (files.length === 0) {
+                      setFieldError(fieldName, errorMessageMapping.UNSUPPORTED)
+                      deleteFile({
+                        variables: { id: values.id },
+                      }).then(({ data }) => {
+                        setFieldValue(
+                          fieldName,
+                          data.removeUploadedManuscript.files,
+                        )
+                      })
+                      return
+                    }
 
-                const [file] = files
-                uploadFile({
-                  variables: { file, id: values.id, fileSize: file.size },
-                }).then(({ data }) => {
-                  setFieldValue('meta.title', data.uploadManuscript.meta.title)
-                  setFieldValue(fieldName, data.uploadManuscript.files)
-                })
-              }}
-            />
-          </Box>
-        </React.Fragment>
+                    const [file] = files
+                    uploadFile({
+                      variables: { file, id: values.id, fileSize: file.size },
+                    }).then(({ data }) => {
+                      setFieldValue(
+                        'meta.title',
+                        data.uploadManuscript.meta.title,
+                      )
+                      setFieldValue(fieldName, data.uploadManuscript.files)
+                    })
+                  }}
+                />
+              </Box>
+            </React.Fragment>
+          )}
+        </Mutation>
       )
     }}
   </Mutation>
