4761cc21e8f135862e12067571c18df8edf1d503
Peter Hooper <diversemix@gmail.com>
2018-11-19 22:20:03 +0000

    Add tests, obfuscation and email checks

---
+++ scripts/obfuscate.js
@@ -0,0 +1,81 @@
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+const crypto = require('crypto')
+const { replace, includes } = require('lodash')
+
+const rootDir = process.cwd()
+
+// Output from " find | grep http-mocks$ "
+const pathHttpMocks = [
+  path.join(rootDir, 'test/http-mocks'),
+  path.join(rootDir, 'server/meca/test/http-mocks'),
+]
+
+const md5 = s =>
+  crypto
+    .createHash('md5')
+    .update(s)
+    .digest('hex')
+
+/*
+ * Walk the tree and replace emailAddresses [.values] by md5'ing
+ */
+const obfuscateMockFiles = async mockPath => {
+  const files = await new Promise((resolve, reject) => {
+    glob(path.join(mockPath, '/**/*'), (err, result) => {
+      if (err) reject(err)
+      else resolve(result)
+    })
+  })
+
+  files.forEach(file => {
+    if (fs.lstatSync(file).isFile()) {
+      const re = new RegExp('"emailAddresses":\\[\\{"value":"([^,]+)",', 'g')
+      const content = fs.readFileSync(file).toString()
+      fs.writeFileSync(file, obfuscate(content, re))
+    }
+  })
+  console.log(`${mockPath} obfuscated ${files.length} files`)
+}
+
+const obfuscateEmail = s => {
+  const re = new RegExp('<email>([^<>]+)</email>', 'g')
+  return obfuscate(s, re)
+}
+
+/*
+ * obfuscates a string with a regular expression defining what needs to be
+ * hashed and replaced.
+ */
+const obfuscate = (s, re) => {
+  let m
+  const matches = []
+  do {
+    m = re.exec(s)
+    if (m) {
+      matches.push({ str: m[1], index: m.index })
+    }
+  } while (m)
+
+  let newStr = s
+
+  matches.forEach(item => {
+    // make sure this is idempotent
+    if (includes(item.str, '.') && includes(item.str, '@')) {
+      newStr = replace(newStr, item.str, md5(item.str))
+    }
+  })
+  return newStr
+}
+
+/*
+ * If run from the terminal then execute obfuscateMockFiles()
+ */
+if (require.main === module) {
+  pathHttpMocks.map(mockPath => obfuscateMockFiles(mockPath))
+}
+
+module.exports = {
+  obfuscateEmail,
+}
---
+++ server/xpub-model/entities/user/helpers/elife-api.test.person.js
@@ -0,0 +1,39 @@
+module.exports = {
+  id: 'ca22ca22',
+  type: {
+    id: 'reviewing-editor',
+    label: 'Reviewing Editor',
+  },
+  name: {
+    surname: 'Surname',
+    givenNames: 'Given Names',
+    preferred: 'Given Names Surname',
+    index: 'Surname, Given Names',
+  },
+  emailAddresses: [
+    {
+      value: 'person@email.com',
+      access: 'restricted',
+    },
+  ],
+  research: {
+    focuses: ['protein synthesis', 'translational control'],
+    expertises: [
+      {
+        id: 'chromosomes-gene-expression',
+        name: 'Chromosomes and Gene Expression',
+      },
+    ],
+  },
+  affiliations: [
+    {
+      name: ['National Institute of Child Health and Human Development'],
+      address: {
+        formatted: ['United States'],
+        components: {
+          country: 'United States',
+        },
+      },
+    },
+  ],
+}
