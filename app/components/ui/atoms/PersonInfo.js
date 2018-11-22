c8449d8b3d31ec0c51b74076fe887a4214ba46e3
Semra Shukrieva <semra.shukrieva@yld.io>
2018-11-22 17:34:53 +0000

    create an atom for person info shown on the modal

---
+++ app/components/ui/atoms/PersonInfo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { H2 } from '@pubsweet/ui'
+import SmallParagraph from './SmallParagraph'
+import Paragraph from './Paragraph'
+
+const PersonInfo = ({ person }) => {
+  const { name, aff, subjectAreas } = person
+  return (
+    <React.Fragment>
+      <H2>{name}</H2>
+      <Paragraph>{aff}</Paragraph>
+      <SmallParagraph>
+        Research focuses: {subjectAreas.join(', ')}
+      </SmallParagraph>
+    </React.Fragment>
+  )
+}
+
+PersonInfo.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    aff: PropTypes.string.isRequired,
+    subjectAreas: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  }).isRequired,
+}
+
+export default PersonInfo
---
+++ app/components/ui/atoms/PersonInfo.md
@@ -0,0 +1,14 @@
+```js
+<PersonInfo
+  person={{
+    id: '1234',
+    name: 'Michael J Frank',
+    aff: 'Max Planck Institute, Germany',
+    subjectAreas: [
+      'genetics of global gene expression',
+      'mouse models of human disease',
+      'haematology',
+    ],
+  }}
+/>
+```
