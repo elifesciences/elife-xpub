e21e307d8e32538dbe3e7dd06e479d2f91ff57c2
Peter Hooper <diversemix@gmail.com>
2018-11-23 13:15:11 +0000

    Add alternatives snapshots for tests to cope with and without secret

--- server/meca/file-generators/article.test.js
+++ server/meca/file-generators/article.test.js
@@ -1,5 +1,6 @@
 const { createTables } = require('@pubsweet/db-manager')
 const path = require('path')
+const fs = require('fs')
 const config = require('config')
 const { db } = require('pubsweet-server')
 const generateXml = require('./article')
@@ -14,6 +15,16 @@ const existingNames = [{ id: 1, first: 'J. Edward', last: 'Reviewer' }]
 
 Replay.fixtures = `${__dirname}/../test/http-mocks`
 
+const getXmlSnapshot = () => {
+  const secret = config.get('server.api.secret')
+  let filename = 'server/meca/file-generators/__snapshots__/article.test.xml'
+  if (!secret) {
+    filename =
+      'server/meca/file-generators/__snapshots__/article.test-no-secret.xml'
+  }
+  return fs.readFileSync(filename).toString()
+}
+
 describe('Article XML generator', () => {
   beforeAll(async () => {
     await createTables(true)
@@ -26,9 +37,7 @@ describe('Article XML generator', () => {
   })
 
   it('generates expected XML', async () => {
-    let xml = await generateXml(sampleManuscript)
-    xml = obfuscateEmail(xml)
-    if (config.get('server.api.secret')) expect(xml).toMatchSnapshot()
-    else expect(xml).toMatchSnapshot('no-secret')
+    const xml = await generateXml(sampleManuscript)
+    expect(obfuscateEmail(xml)).toBe(getXmlSnapshot())
   })
 })
