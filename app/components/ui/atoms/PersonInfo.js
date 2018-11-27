0465f3bbbcde77be7ceaa5ecdc273f928e37a9a9
Jennifer Spencer <jennifer.spencer@yld.io>
2018-11-27 15:10:08 +0000

    style(PersonInfo): remove margin under name & add extra margin under focuses

--- app/components/ui/atoms/PersonInfo.js
+++ app/components/ui/atoms/PersonInfo.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { th } from '@pubsweet/ui-toolkit'
 import { H2 } from '@pubsweet/ui'
 import SmallParagraph from './SmallParagraph'
 import Paragraph from './Paragraph'
 
+const StyledH2 = styled(H2)`
+  margin-bottom: 0;
+`
+
+const StyledSmallParagraph = styled(SmallParagraph)`
+  margin-bottom: ${th('space.4')};
+`
+
 const PersonInfo = ({ name, institution, focuses, expertises }) => (
   <React.Fragment>
-    <H2>{name}</H2>
+    <StyledH2>{name}</StyledH2>
     <Paragraph>{institution}</Paragraph>
-    <SmallParagraph>Expertises: {expertises.join(', ')}</SmallParagraph>
-    <SmallParagraph secondary>
+    <SmallParagraph>Expertise: {expertises.join(', ')}</SmallParagraph>
+    <StyledSmallParagraph secondary>
       Research focuses: {focuses.join(', ')}
-    </SmallParagraph>
+    </StyledSmallParagraph>
   </React.Fragment>
 )
 
