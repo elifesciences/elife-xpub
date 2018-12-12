87fac92cc8417db154ac14fad9cf064a66d8a4ac
Peter Hooper <diversemix@gmail.com>
2018-12-12 15:07:15 +0000

    Add configurable auth to mailer.js

--- config/non-serializable/mailer.js
+++ config/non-serializable/mailer.js
@@ -3,4 +3,8 @@ module.exports = {
     host: `${process.env.MAILER_HOST || 'localhost'}`,
     port: `${process.env.MAILER_PORT || 25}`,
   },
+  auth: {
+    user: `${process.env.MAILER_AUTH_USER || 'user'}`,
+    pass: `${process.env.MAILER_AUTH_PASSWORD || 'password'}`,
+  },
 }
