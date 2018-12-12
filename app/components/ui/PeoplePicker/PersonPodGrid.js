eef2a99887ecd95175cd8fcdde24bbf537e82ceb
Semra Shukrieva <semra.shukrieva@yld.io>
2018-12-12 17:20:59 +0000

    add windowScroller

--- app/components/ui/PeoplePicker/PersonPodGrid.js
+++ app/components/ui/PeoplePicker/PersonPodGrid.js
@@ -3,71 +3,73 @@ import PropTypes from 'prop-types'
 import { List, WindowScroller } from 'react-virtualized'
 
 import { peoplePropType } from './types'
-// import PersonPod from './PersonPod'
+import PersonPod from './PersonPod'
 
-// const Pod = ({
-//   person,
-//   isSelected,
-//   toggleSelection,
-//   selection,
-//   maxSelection,
-// }) => (
-//   <PersonPod
-//     expertises={person.expertises}
-//     focuses={person.focuses}
-//     institution={person.aff}
-//     isKeywordClickable={false}
-//     isSelectButtonClickable={
-//       isSelected(person) || selection.length < maxSelection
-//     }
-//     isSelected={isSelected(person)}
-//     key={person.id}
-//     name={person.name}
-//     selectButtonType={isSelected(person) ? 'selected' : 'add'}
-//     togglePersonSelection={() => toggleSelection(person)}
-//   />
-// )
+const Pod = ({
+  person,
+  isSelected,
+  toggleSelection,
+  selection,
+  maxSelection,
+}) => (
+  <PersonPod
+    expertises={person.expertises}
+    focuses={person.focuses}
+    institution={person.aff}
+    isKeywordClickable={false}
+    isSelectButtonClickable={
+      isSelected(person) || selection.length < maxSelection
+    }
+    isSelected={isSelected(person)}
+    key={person.id}
+    name={person.name}
+    selectButtonType={isSelected(person) ? 'selected' : 'add'}
+    togglePersonSelection={() => toggleSelection(person)}
+  />
+)
 
-class PersonPodGrid extends React.PuroComponent {
-  constructor(props) {
-    super(props)
-    this.state = {}
-    this.setRef = this.setRef.bind(this)
-    this.toggleView = this.toggleView.bind(this)
-  }
-
-  rowRenderer = ({ index, key, style, parent }) => (
-    <div key={key} style={style}>
-      <p> {index} </p>
-    </div>
-  )
-
-  setRef = ref => (this.windowScroller = ref)
-
-  toggleView() {
-    this.setState(
-      {
-        show: !this.state.show,
-      },
-      () => {
-        this.windowScroller.updatePosition()
-      },
+class PersonPodGrid extends React.Component {
+  rowRenderer = ({ index, key, style }) => {
+    const {
+      people,
+      isSelected,
+      toggleSelection,
+      selection,
+      maxSelection,
+    } = this.props
+    return (
+      <div key={key} style={style}>
+        <Pod
+          isSelected={isSelected}
+          maxSelection={maxSelection}
+          person={people[index]}
+          selection={selection}
+          toggleSelection={toggleSelection}
+        />
+      </div>
     )
   }
 
   render() {
     return (
-      <WindowScroller ref={this.setRef} scrollElement={window}>
-        {({ height, scrollTop }) => (
-          <div>
+      <WindowScroller
+      // onScroll={() => console.log('Scrolled')}
+      // ref={el => (this.ref = el)}
+      // scrollElement={window}
+      >
+        {({ height, isScrolling, onChildScroll, scrollTop, registerChild }) => (
+          <div ref={registerChild}>
             <List
               autoHeight
               height={height}
-              rowCount={1000}
-              rowHeight={30}
+              isScrolling={isScrolling}
+              onScroll={onChildScroll}
+              overscanRowCount={2}
+              rowCount={this.props.people.length}
+              rowHeight={100}
               rowRenderer={this.rowRenderer}
-              scrollTop={scrollTop}
-              width={200}
+              scrollTop={this.scrollY}
+              width={500}
             />
           </div>
         )}
