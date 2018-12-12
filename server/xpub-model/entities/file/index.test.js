38d9e780b5aa2134b8053b0a7b27d8f87825df46
jyenes <javier.yenes@yld.io>
2018-12-12 13:33:23 +0100

    fix xpub-model file tests

--- server/xpub-model/entities/file/index.test.js
+++ server/xpub-model/entities/file/index.test.js
@@ -1,76 +1,32 @@
 const { createTables } = require('@pubsweet/db-manager')
 const File = require('.')
-const config = require('config')
-const startS3rver = require('../../../xpub-server/test/mock-s3-server')
-
-class DummyFile extends File {
-  get storage() {
-    return {
-      putContent: () => {},
-      getContent: () => {},
-      deleteObject: () => ({
-        promise: () => Promise.resolve(true),
-      }),
-    }
-  }
-}
 
 describe('Manuscript', () => {
   beforeEach(() => createTables(true))
 
   describe('delete File', () => {
-    let s3Server
-
-    beforeEach(async () => {
-      const server = await startS3rver({
-        ...config.get('aws.credentials'),
-        ...config.get('aws.s3'),
-      })
-      s3Server = server.instance
-    })
-
-    afterEach(done => {
-      s3Server.close(done)
-    })
-
-    it('should fail if id is not provided when deleting content', async () => {
-      const file = new DummyFile()
+    it('should fail if id is not provided when deleting the file', async () => {
+      const file = new File()
       let response
-      const error = new Error('File has no ID, or is invalid')
+      const error = new Error(
+        "one of the identifier columns [id] is null or undefined. Have you specified the correct identifier column for the model 'File' using the 'idColumn' property?",
+      )
       try {
-        response = await file.deleteContent()
+        response = await file.delete()
       } catch (err) {
         response = err
       }
       expect(response).toEqual(error)
     })
 
-    it('if should delete the s3 content', async () => {
-      const file = new DummyFile({
-        manuscriptId: 1,
-        id: 1,
+    it('if should delete the file', async () => {
+      const file = new File({
+        manuscriptId: '1',
+        id: 'c99c5243-65fb-4679-9672-ab8cb2bd7653',
         url: '/an/url',
       })
-      const response = await file.deleteContent()
+      const response = await file.delete()
       expect(response).toBeTruthy()
-    })
-
-    it('if should return an error deleting the s3 content, if there is no access to S3', async () => {
-      jest
-        .spyOn(DummyFile.prototype, 'deleteContent')
-        .mockImplementationOnce(() =>
-          Promise.reject(new Error('Failed to persist file')),
-        )
-      const file = new DummyFile({
-        manuscriptId: 1,
-        id: 1,
-        url: '/an/url',
-      })
-      try {
-        await file.deleteContent()
-      } catch (error) {
-        expect(error.message).toBe('Failed to persist file')
-      }
     })
   })
 })
