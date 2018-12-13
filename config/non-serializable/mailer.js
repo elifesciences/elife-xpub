93465be51bd9e5404366ddf7bc16515ed1616c12
Peter Hooper <diversemix@gmail.com>
2018-12-13 11:33:03 +0000

    Allow the auth to be configurable

--- config/non-serializable/mailer.js
+++ config/non-serializable/mailer.js
@@ -2,6 +2,6 @@ module.exports = {
   transport: {
     host: `${process.env.MAILER_HOST || 'localhost'}`,
     port: `${process.env.MAILER_PORT || 25}`,
+    auth: JSON.parse(`${process.env.MAILER_AUTH || '{}'}`),
   },
-  auth: `${process.env.MAILER_AUTH || '{}'}`,
 }
