c3fe163193f9908f8d6ef5dc464a3d8e936f9aba
jyenes <javier.yenes@yld.io>
2018-12-13 14:40:36 +0100

    fixing the scrolling with react-virtualized

--- app/components/ui/PeoplePicker/PersonPodGrid.js
+++ app/components/ui/PeoplePicker/PersonPodGrid.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { List, WindowScroller } from 'react-virtualized'
+import { List } from 'react-virtualized'
 
 import { peoplePropType } from './types'
 import PersonPod from './PersonPod'
@@ -49,31 +49,21 @@ class PersonPodGrid extends React.Component {
       </div>
     )
   }
-
   render() {
+    const rowHeight = 100
+    const rowWidth = 800
+    const height = 1000
+
     return (
-      <WindowScroller
-      // onScroll={() => console.log('Scrolled')}
-      // ref={el => (this.ref = el)}
-      // scrollElement={window}
-      >
-        {({ height, isScrolling, onChildScroll, scrollTop, registerChild }) => (
-          <div ref={registerChild}>
-            <List
-              autoHeight
-              height={height}
-              isScrolling={isScrolling}
-              onScroll={onChildScroll}
-              overscanRowCount={2}
-              rowCount={this.props.people.length}
-              rowHeight={100}
-              rowRenderer={this.rowRenderer}
-              scrollTop={this.scrollY}
-              width={500}
-            />
-          </div>
-        )}
-      </WindowScroller>
+      <div>
+        <List
+          height={height}
+          rowCount={this.props.people.length}
+          rowHeight={rowHeight}
+          rowRenderer={this.rowRenderer}
+          width={rowWidth}
+        />
+      </div>
     )
   }
 }
