d340a8d1f156400b9d5c3b3859d1e0ff518984e9
Giorgio Sironi <g.sironi@elifesciences.org>
2018-12-21 11:55:33 +0000

    Use a single Application fixture for all tests

--- test/helpers/set-fixture-hooks.js
+++ test/helpers/set-fixture-hooks.js
@@ -1,7 +1,12 @@
 import { startServer, setup, teardown } from './setup'
 
+const f = fixture('Application')
+f
+  .before(startServer)
+  .beforeEach(setup)
+  .afterEach(teardown)
+
+// TODO: remove
 export default chosenFixture =>
   chosenFixture
-    .before(startServer)
-    .beforeEach(setup)
-    .afterEach(teardown)
+
