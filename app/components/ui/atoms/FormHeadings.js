bf24e9bdd5a880d4383c7ee8fe4bc379f122fea5
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2018-12-27 16:06:24 +0000

    replacing deprecated .extend function

--- app/components/ui/atoms/FormHeadings.js
+++ app/components/ui/atoms/FormHeadings.js
@@ -1,11 +1,12 @@
+import styled from "styled-components";
 import { H2, H3 } from '@pubsweet/ui'
 
-export const FormH2 = H2.extend`
+export const FormH2 = styled(H2)`
   padding-top: 0px;
   margin-bottom: 36px;
 `
 
-export const FormH3 = H3.extend`
+export const FormH3 = styled(H3)`
   padding-top: 0px;
   margin-bottom: 24px;
 `
