45c10935b2d6ecbf578c531de721ccbc4e465fb9
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2019-01-03 11:15:33 +0000

    Revert "Merge pull request #1219 from elifesciences/1080-hotjar-feedback"
    This reverts commit b41b4a372be6f9e8466b939be72f1aab7ad04dc2, reversing
    changes made to bc393e9b672f0f98a68e2a7a39374b25ee920d35.

--- app/components/pages/ThankYou/ThankYou.js
+++ app/components/pages/ThankYou/ThankYou.js
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 import { Box } from 'grid-styled'
 import PropTypes from 'prop-types'
 import { H1 } from '@pubsweet/ui'
-import { hotjar } from 'react-hotjar'
-
 import NativeLink from '../../ui/atoms/NativeLink'
 import ButtonLink from '../../ui/atoms/ButtonLink'
 import Paragraph from '../../ui/atoms/Paragraph'
@@ -20,42 +18,25 @@ const BookmarkLink = styled(NativeLink)`
   clear: both;
 `
 
-const hotJarId = 443270
-const hotjarSnippetVersion = 6
-
-class ThankYou extends React.Component {
-  componentDidMount() {
-    window.hj('trigger', 'thank_you_page_hotjar_feedback')
-  }
-
-  shouldComponentUpdate() {
-    return false
-  }
-
-  render() {
-    hotjar.initialize(hotJarId, hotjarSnippetVersion)
-    const { title } = this.props
-    return (
-      <CenteredContent mx="auto" width={[1, 1, 1, 600]}>
-        <H1>Thank you</H1>
-        <Paragraph>
-          Your submission, &quot;
-          {title}
-          &quot; has been received.
-        </Paragraph>
-        <Paragraph>You will be informed of a decision soon.</Paragraph>
-        <SubText>
-          You may want to bookmark the link below to check the progress of your
-          submission.
-          <BookmarkLink href="/">elifesciences.org/submissions</BookmarkLink>
-        </SubText>
-        <ButtonLink data-test-id="finish" primary to="/">
-          Finish
-        </ButtonLink>
-      </CenteredContent>
-    )
-  }
-}
+const ThankYou = ({ title }) => (
+  <CenteredContent mx="auto" width={[1, 1, 1, 600]}>
+    <H1>Thank you</H1>
+    <Paragraph>
+      Your submission, &quot;
+      {title}
+      &quot; has been received.
+    </Paragraph>
+    <Paragraph>You will be informed of a decision soon.</Paragraph>
+    <SubText>
+      You may want to bookmark the link below to check the progress of your
+      submission.
+      <BookmarkLink href="/">elifesciences.org/submissions</BookmarkLink>
+    </SubText>
+    <ButtonLink data-test-id="finish" primary to="/">
+      Finish
+    </ButtonLink>
+  </CenteredContent>
+)
 
 ThankYou.propTypes = {
   title: PropTypes.string.isRequired,
