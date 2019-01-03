950bc42bf6c5294089d5ddc812a70a5d1f6662cd
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2019-01-03 16:36:13 +0000

    removed unused import

--- app/components/pages/SubmissionWizard/steps/Submission/SubjectAreaDropdown.test.js
+++ app/components/pages/SubmissionWizard/steps/Submission/SubjectAreaDropdown.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { mount } from 'enzyme'
 import theme from '@elifesciences/elife-theme'
 
 import SubjectAreaDropdown from './SubjectAreaDropdown'
