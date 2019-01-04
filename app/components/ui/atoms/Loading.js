c8948a1195a212c9cd17621f4182d7adc8c6c05f
Semra Shukrieva <semra.shukrieva@yld.io>
2019-01-04 14:42:46 +0000

    add Loading to the styleguide

--- app/components/ui/atoms/Loading.js
+++ app/components/ui/atoms/Loading.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-
-import { colorBorder, colorPrimary } from '../../../../client/elife-theme/src'
+import { th } from '@pubsweet/ui-toolkit'
 
 const Spinner = styled.div`
   height: ${props => props.size}px;
   width: ${props => props.size}px;
-  border-top: #0288d1;
-  border-width: ${props => props.lineThinkess}px;
+  border-width: ${props => props.lineThickness}px;
   border-radius: 50%;
   border-style: solid;
   display: block;
   flex-shrink: 0;
-  border-color: ${colorPrimary};
-  border-top-color: ${colorBorder};
+  border-color: ${th('colorBorder')};
+  border-top-color: ${th('colorPrimary')};
   animation-timing-function: linear;
   animation-iteration-count: infinite;
   animation-duration: 750ms;
-  animation-name: rotating @keyframes rotating {
+  animation-name: rotating;
+  @keyframes rotating {
     from {
       transform: rotate(0deg);
     }
@@ -28,17 +27,17 @@ const Spinner = styled.div`
   }
 `
 
-const Loading = ({ size, lineThinkess }) => (
-  <Spinner lineThinkess={lineThinkess} size={size} />
+const Loading = ({ size, lineThickness }) => (
+  <Spinner lineThickness={lineThickness} size={size} />
 )
 
 Loading.propTypes = {
   size: PropTypes.number,
-  lineThinkess: PropTypes.number,
+  lineThickness: PropTypes.number,
 }
 
 Loading.defaultProps = {
   size: 72,
-  lineThinkess: 1,
+  lineThickness: 4,
 }
 export default Loading
