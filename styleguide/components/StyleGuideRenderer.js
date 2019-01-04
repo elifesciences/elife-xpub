a0504de89dc4db81a25ba5bad7c921c38036227e
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2019-01-04 11:55:23 +0000

    adding GlobalStyle to wrapper

--- styleguide/components/StyleGuideRenderer.js
+++ styleguide/components/StyleGuideRenderer.js
@@ -1,6 +1,3 @@
 import DefaultRenderer from 'react-styleguidist/lib/rsg-components/StyleGuide/StyleGuideRenderer'
-import { injectGlobalStyles } from 'pubsweet-client/src/helpers/StyleRoot'
-
-injectGlobalStyles()
 
 export default DefaultRenderer
