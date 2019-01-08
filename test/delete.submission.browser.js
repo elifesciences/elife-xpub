7a88a36362f8bddd6f728802d47442405c48088c
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2019-01-08 17:04:33 +0000

    Revert "two empty tests"
    This reverts commit d3025a3a953688b8ddd10d4e9b59f39bd231adf9.

--- test/delete.submission.browser.js
+++ test/delete.submission.browser.js
@@ -1,9 +1,60 @@
-const f = fixture('DeleteSubmission')
+import config from 'config'
+import { Selector } from 'testcafe'
+import { author, dashboard, login, wizardStep } from './pageObjects'
+import setFixtureHooks from './helpers/set-fixture-hooks'
 
-f.before(() => Promise.resolve())
-  .beforeEach(() => Promise.resolve())
-  .afterEach(() => Promise.resolve())
+const f = fixture('DeleteSubmission')
+setFixtureHooks(f)
 
 test('Delete a Submission', async t => {
-  await Promise.resolve()
+  console.log('tests/delete.submission.browser.js start')
+  await t
+    .navigateTo(login.url)
+    .click(login.button)
+    .click(dashboard.desktopNewSubmission)
+
+  // author details initially empty
+  console.log('tests/delete.submission.browser.js author details')
+  await t
+    .expect(author.firstNameField.value)
+    .eql('')
+    .expect(author.secondNameField.value)
+    .eql('')
+    .expect(author.emailField.value)
+    .eql('')
+    .expect(author.institutionField.value)
+    .eql('')
+
+  // create a new submission
+  console.log('tests/delete.submission.browser.js create a new submission')
+  await t
+    .click(author.orcidPrefill)
+    .expect(author.firstNameField.value)
+    .eql('Tamlyn')
+    .expect(author.secondNameField.value)
+    .eql('Rhodes')
+    .expect(author.emailField.value)
+    .eql('f72c502e0d657f363b5f2dc79dd8ceea')
+    .expect(author.institutionField.value)
+    .eql('Tech team, University of eLife')
+    .selectText(author.emailField)
+    .typeText(author.emailField, 'example@example.org')
+    .click(wizardStep.next)
+
+  // navigate back to the dashboard page and cancel the delete the submission
+  console.log('tests/delete.submission.browser.js cancel')
+  await t
+    .navigateTo(`${config.get('pubsweet-server.baseUrl')}`)
+    .click(dashboard.trashButton)
+    .click(Selector('[data-test-id=cancel'))
+    .expect(dashboard.trashButton.count)
+    .eql(1)
+
+  // navigate back to the dashboard page and accept the delete the submission
+  console.log('tests/delete.submission.browser.js accept')
+  await t
+    .click(dashboard.trashButton)
+    .click(Selector('[data-test-id=accept'))
+    .expect(dashboard.trashButton.count)
+    .eql(0)
 })
