9dc9223c61eabb2c1f204a2c975a1293fc903bff
Semra Shukrieva <semra.shukrieva@yld.io>
2019-01-10 22:51:35 +0000

    fix typo

--- app/components/pages/Dashboard/EjpLink.js
+++ app/components/pages/Dashboard/EjpLink.js
@@ -4,20 +4,20 @@ import styled from 'styled-components'
 import Paragraph from '../../ui/atoms/Paragraph'
 import NativeLink from '../../ui/atoms/NativeLink'
 
-const CenterdSmallParagraph = styled(Paragraph.Small).attrs({
+const CenteredSmallParagraph = styled(Paragraph.Small).attrs({
   secondary: true,
 })`
   text-align: center;
 `
 
 const EjpLink = () => (
-  <CenterdSmallParagraph>
+  <CenteredSmallParagraph>
     Can&#39;t find a submission? You might find it in our full{' '}
     <NativeLink href="https://submit.elifesciences.org">
       peer review and submissions
     </NativeLink>{' '}
     system
-  </CenterdSmallParagraph>
+  </CenteredSmallParagraph>
 )
 
 export default EjpLink
