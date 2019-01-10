8115483b8327aa966116f388d37951374da7c264
Peter <diversemix@gmail.com>
2019-01-10 13:27:49 +0000

    Update the manifest.xml to contain references to the supplementary files (#1274)
    * Update the manifest.xml to contain references to the supplementary files
    
    * Remove spurious comment

--- server/meca/file-generators/manifest.js
+++ server/meca/file-generators/manifest.js
@@ -1,7 +1,40 @@
 const fs = require('fs-extra')
 
-async function generateManifest() {
-  return fs.readFile(`${__dirname}/manifest.xml`, 'utf8')
+function supplementaryXml(files) {
+  const keyList = ['id', 'mimeType', 'filename']
+  const fileList = files.filter(
+    fileObject => fileObject.type === 'SUPPORTING_FILE',
+  )
+
+  let supplementaryFileXml = ''
+  const template = fs.readFileSync(
+    `${__dirname}/manifest-supplementary-file.xml`,
+    'utf8',
+  )
+
+  fileList.forEach(file => {
+    supplementaryFileXml += keyList.reduce(
+      (xml, key) => xml.replace(new RegExp(`{${key}}`, 'g'), file[key]),
+      template,
+    )
+  })
+
+  return supplementaryFileXml
+}
+
+function manifestXml(suppFilesXml) {
+  const template = fs.readFileSync(`${__dirname}/manifest.xml`, 'utf8')
+  const xml = template.replace('{supplementaryFiles}', suppFilesXml)
+
+  return xml
+}
+
+function generateManifest(files) {
+  if (!Array.isArray(files)) {
+    throw TypeError(`Expecting array: ${typeof files}`)
+  }
+
+  return manifestXml(supplementaryXml(files))
 }
 
 module.exports = generateManifest
