290fcfc1b845c9feebe9dfd75eaa9e11db5146c6
SemraSh <32770804+SemraSh@users.noreply.github.com>
2019-01-11 14:28:18 +0000

    1122 Fix fonts against designs (#1277)
    * delete the small paragraph to be added to paragraph
    
    * create different styles of paragraphs
    
    * replace all the paragraphs with the new structure
    
    * replace Writing paragraphs with Reading ones in the author reading pages
    
    * change the styleguide to include the new paragraph structure
    
    * fix typo
    
    * remove extra paragraph used in a static page
    
    * Paragraph.Small changed to noto sans
    
    * fix the No ORCID message to have noto serif as the rest of the page

--- app/components/ui/molecules/DashboardList.js
+++ app/components/ui/molecules/DashboardList.js
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 import { Box } from '@rebass/grid'
 import { th } from '@pubsweet/ui-toolkit'
 
-import SmallParagraph from '../../ui/atoms/SmallParagraph'
 import DashboardListItem from '../../ui/molecules/DashboardListItem'
+import Paragraph from '../atoms/Paragraph'
 
 const EmptyListMessage = styled(Box)`
   text-align: center;
   color: ${th('colorTextSecondary')};
 `
-const EmptyListSmallParagraph = styled(SmallParagraph)`
+const EmptyListSmallParagraph = styled(Paragraph.Small)`
   font-family: ${th('fontInterface')};
 `
 const EmptyListParagraph = styled(Box)`
