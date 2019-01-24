7ce5f5c6a14a176e76742d62f8cf52a21b05d7b7
Semra Shukrieva <semra.shukrieva@yld.io>
2019-01-24 00:23:21 +0000

    fix test id

--- test/pageObjects/files.js
+++ test/pageObjects/files.js
@@ -9,7 +9,7 @@ const files = {
   manuscriptUpload: Selector('[data-test-id=upload]>input'),
   supportingFilesUpload: Selector('[data-test-id=supportingFilesUpload]>input'),
   supportingFilesRemove: Selector('[data-test-id=supportingFilesRemove]'),
-  supportingFile: Selector('[data-test-id=file_block_name]'),
+  supportingFile: Selector('[data-test-id=file-block-name]'),
   fileName: Selector('[data-test-id=fileName]'),
   dropzoneMessage: Selector('[data-test-id=dropzoneMessage]'),
 }
