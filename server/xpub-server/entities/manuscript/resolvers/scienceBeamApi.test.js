ea4a88765c0f30fc4826cc7a64e6197f2a1519fe
Erez Odier <erez.odier@gmail.com>
2019-02-05 21:43:56 +0000

    Use async

--- server/xpub-server/entities/manuscript/resolvers/scienceBeamApi.test.js
+++ server/xpub-server/entities/manuscript/resolvers/scienceBeamApi.test.js
@@ -562,14 +562,14 @@ require('request-promise-native')
 const { extractSemantics } = require('./scienceBeamApi')
 
 describe('scienceBeamApi', () => {
-  it('extracts correct data', () => {
-    const config = jest.fn(() => ({
+  it('extracts correct data', async () => {
+    const config = {
       get(key) {
         return ''
       },
-    }))
+    }
 
-    const title = extractSemantics(config, '', '', '')
+    const title = await extractSemantics(config, '', '', '')
 
     expect(title).toBe(
       'The Relationship Between Lamport Clocks and Interrupts Using Obi',
