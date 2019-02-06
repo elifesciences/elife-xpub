769aae0aec4962a476eac018ac8e91ec4cbaf712
Semra Shukrieva <semra.shukrieva@yld.io>
2019-02-06 11:02:11 +0000

    add the wabpack configuration of module resolve

--- webpack/common-resolve.js
+++ webpack/common-resolve.js
@@ -1,7 +1,11 @@
+const path = require('path')
+
 module.exports = {
   // symlinks: false, // needed so that babel doesn't look for plugins in components
   alias: {
     joi: 'joi-browser',
+    ui: path.resolve(__dirname, '..', 'app', 'components', 'ui'),
   },
   extensions: ['.js', '.jsx'],
+  // modules: [path.join(__dirname, 'app', 'components', 'ui'), 'node_modules'],
 }
