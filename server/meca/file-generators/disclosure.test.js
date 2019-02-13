321d2e49e6c445d786ed09a984c2d479bf0c4e5d
Peter Hooper <diversemix@gmail.com>
2019-02-13 00:07:23 +0000

    Add test and fix issue

--- server/meca/file-generators/disclosure.test.js
+++ server/meca/file-generators/disclosure.test.js
@@ -1,36 +1,41 @@
 const PDFParser = require('pdf2json')
 const generatePdf = require('./disclosure')
-const fs = require('fs')
 
 describe('Disclosure PDF generator', () => {
   it('returns a string of a PDF', async () => {
     const document = await generatePdf({
       meta: {},
-      teams: [{ role: 'author', teamMembers: [{ alias: {} }] }],
-    })
-
-    // save the document
-    //
-    fs.writeFileSync('./disc.pdf', document, err => {
-      if (err) {
-        return console.log(err)
-      }
-      return true
+      teams: [
+        {
+          role: 'author',
+          teamMembers: [
+            {
+              alias: {
+                givenNames: 'J. Edward',
+                surname: 'XXYYZZ',
+              },
+            },
+          ],
+        },
+      ],
     })
 
     const pdfParser = new PDFParser()
     let errors = 0
     pdfParser.on('pdfParser_dataError', err => {
-      errors += 1
-      console.log(err)
+      if (err) errors += 1
     })
 
-    expect(() => pdfParser.loadPDF('./disc.pdf')).not.toThrow()
-    // console.log(JSON.stringify(pdfParser, null, 4))
+    expect(() => pdfParser.parseBuffer(document)).not.toThrow()
+    pdfParser.on('pdfParser_dataReady', pdfData => {
+      expect(errors).toBe(0)
+      expect(pdfParser.data).not.toBe(null)
 
-    expect(document.substring(0, 6)).toBe('%PDF-1')
-    expect(errors).toBe(0)
-    expect(pdfParser.data).not.toBe(null)
-    expect(pdfParser.PDFJS.pdfDocument).not.toBe(null)
+      const jsonData = JSON.stringify(pdfParser.data)
+
+      expect(jsonData.includes('Our%20privacy%20policy')).toBe(true)
+      expect(jsonData.includes('Edward')).toBe(true)
+      expect(jsonData.includes('XXYYZZ')).toBe(true)
+    })
   })
 })
