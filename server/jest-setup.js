15120d6f65ccb676986e2e50e176bbe235e9c198
Peter Hooper <diversemix@gmail.com>
2019-02-19 09:36:57 +0000

    Add reference link into comment

--- server/jest-setup.js
+++ server/jest-setup.js
@@ -4,6 +4,7 @@ const replaySetup = require('../test/helpers/replay-setup')
 replaySetup('success')
 
 // The pipeline complains at the default of 5s - so double it!
+// https://jestjs.io/docs/en/jest-object#jestsettimeouttimeout
 jest.setTimeout(10000)
 
 jest.mock('@pubsweet/component-send-email', () => ({
