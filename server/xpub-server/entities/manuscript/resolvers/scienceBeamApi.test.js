5921f321ea5bdc3025ea8952836821cad57bacfc
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2019-02-26 09:14:12 +0000

    Reintroduce "Science beam api test (#1448)"
    Revert "Revert "Science beam api test (#1448)" (#1578)"
    
    This reverts commit 7160f48797377c1dcefb81959fcca8d8eecb021d.

---
+++ server/xpub-server/entities/manuscript/resolvers/scienceBeamApi.test.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+jest.mock('request-promise-native', () => ({
+  post: (url, obj) => {
+    const fileContents = fs.readFileSync(`${__dirname}/article.xml`)
+    return fileContents
+  },
+}))
+
+require('request-promise-native')
+const { extractSemantics } = require('./scienceBeamApi')
+
+describe('scienceBeamApi', () => {
+  it('extracts correct data', async () => {
+    const config = {
+      get(key) {
+        return ''
+      },
+    }
+
+    const title = await extractSemantics(config, '', '', '')
+
+    expect(title).toBe(
+      'The Relationship Between Lamport Clocks and Interrupts Using Obi',
+    )
+  })
+})
