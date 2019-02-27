9b3d638fbe065f661a877b34e79480244a3e351e
Javier <javi.yenes@gmail.com>
2019-02-27 13:29:42 +0000

    extract the common code, to a module in order to be reusable on the rest of the test (#1589)
    * creating navigationHelper who will help to refactor and reuse the code along the rest of the test
    
    * adding common module to be reused on the rest of the test
    
    * remove invalid module

---
+++ test/helpers/navigationHelper.js
@@ -0,0 +1,111 @@
+import {
+  author,
+  dashboard,
+  disclosure,
+  editors,
+  files,
+  login,
+  submission,
+  thankyou,
+  wizardStep,
+} from '../pageObjects'
+
+class NavigationHelper {
+  constructor(t) {
+    this.t = t
+  }
+
+  async navigateForward() {
+    await this.t.click(wizardStep.next)
+  }
+
+  async wait(time) {
+    await this.t.wait(time)
+  }
+
+  async login() {
+    await this.t.navigateTo(login.url).click(login.button)
+  }
+
+  async newSubmission() {
+    await this.t.click(dashboard.desktopNewSubmission)
+  }
+
+  async preFillAuthorDetailsWithOrcid() {
+    await this.t.click(author.orcidPrefill)
+  }
+
+  async setAuthorEmail(email) {
+    await this.t
+      .selectText(author.emailField)
+      .typeText(author.emailField, email)
+  }
+
+  async fillCoverletter(text) {
+    await this.t.typeText(files.editor, text)
+  }
+
+  async uploadManuscript(manuscript) {
+    await this.t.setFilesToUpload(files.manuscriptUpload, manuscript.file)
+  }
+
+  async uploadSupportingFiles(supportingFiles) {
+    await this.t.setFilesToUpload(files.supportingFilesUpload, supportingFiles)
+  }
+
+  async addManuscriptMetadata() {
+    await this.t
+      .click(submission.articleType)
+      .click(submission.articleTypes.nth(0))
+      .click(submission.subjectAreaLabel)
+      .pressKey('enter')
+      .pressKey('down')
+      .pressKey('enter')
+      .click(submission.discussionCheckbox)
+      .typeText(submission.discussionText, 'Spoke to Bob about another article')
+      .click(submission.previousArticleCheckbox)
+      .typeText(submission.previousArticleText, 'A title')
+      .click(submission.cosubmissionCheckbox)
+      .typeText(submission.firstCosubmissionTitle, 'Another title')
+      .click(submission.moreSubmission)
+      .typeText(submission.secondCosubmissionTitle, 'Yet another title')
+  }
+
+  async openEditorsPicker() {
+    await this.t.click(editors.suggestedSeniorEditorSelection)
+  }
+
+  async openReviewerPicker() {
+    await this.t.click(editors.suggestedReviewingEditorSelection)
+  }
+
+  async closePeoplePicker() {
+    await this.t.click(editors.peoplePickerSubmit)
+  }
+
+  async selectPeople(people) {
+    people.forEach(async person => {
+      await this.t.click(editors.peoplePickerOptions.nth(person))
+    })
+  }
+
+  async consentDisclosure() {
+    await this.t
+      .typeText(disclosure.submitterName, 'Joe Bloggs')
+      .click(disclosure.consentCheckbox)
+  }
+
+  async submit() {
+    await this.t.click(wizardStep.submit)
+  }
+
+  async accept() {
+    await this.t.click(wizardStep.accept)
+  }
+
+  async thankyou() {
+    await this.t.click(thankyou.finish)
+  }
+}
+
+export default NavigationHelper
