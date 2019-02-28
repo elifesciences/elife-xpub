877d6b88cc1f77b42e5644881a51f384e26f2c2e
jyenes <javier.yenes@yld.io>
2019-02-28 14:19:50 +0000

    adding default case

--- app/trackedRoute.js
+++ app/trackedRoute.js
@@ -5,7 +5,7 @@ import config from 'config'
 
 export default class TrackedRoute extends React.Component {
   trackPage(page) {
-    document.title = config.titles[page.split('/')[1]]
+    document.title = config.titles[page.split('/')[1]] || 'eLife'
     this.lastTracked = page
     ReactGA.set({
       page,
