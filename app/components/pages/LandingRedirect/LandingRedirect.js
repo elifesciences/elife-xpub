43c3a32ebc5c3201b60e7d9724b33cd1cb2737a7
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2019-02-28 10:08:12 +0000

    re-enabled linting and fixed violations

--- app/components/pages/LandingRedirect/LandingRedirect.js
+++ app/components/pages/LandingRedirect/LandingRedirect.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import { H1 } from '@pubsweet/ui'
 
-// eslint-disable-next-line react/prefer-stateless-function
-class LandingRedirect extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return <H1>We&#39;re redirecting you</H1>
-  }
-}
+const LandingRedirect = () => (
+  <H1>We&#39;re redirecting you</H1>
+)
 
 export default LandingRedirect
