4a9dfd94873e833aae4e1de69fdc95cedb8adf14
jyenes <javier.yenes@yld.io>
2019-02-28 14:13:27 +0000

    update titles

--- app/trackedRoute.js
+++ app/trackedRoute.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import ReactGA from 'react-ga'
+import config from 'config'
 
 export default class TrackedRoute extends React.Component {
   trackPage(page) {
+    document.title = config.titles[page.split('/')[1]]
     this.lastTracked = page
     ReactGA.set({
       page,
