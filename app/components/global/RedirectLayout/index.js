4379363319cfdf8eed9a76b6e3c9f11fe3532498
Cory <c.donkin@elifesciences.org>
2019-03-01 16:37:30 +0000

    add text and link button to page

--- app/components/global/RedirectLayout/index.js
+++ app/components/global/RedirectLayout/index.js
@@ -3,19 +3,27 @@ import styled from 'styled-components'
 import { Box } from '@rebass/grid'
 import { th } from '@pubsweet/ui-toolkit'
 import AppBarContainer from 'ui/atoms/AppBarContainer'
-import Centerer from '../layout/Centerer'
 
 const LogoWrapper = styled.span`
   ${th('customMixins.logoHolder')};
 `
+
+const SmallCenterer = styled(Box).attrs({
+  mx: 'auto',
+  px: 3,
+  width: [1, 1, 1, 1000],
+})`
+  min-width: 0;
+`
+
 const RedirectLayout = ({ children }) => (
   <Box pb={5}>
-    <AppBarContainer>
+    <AppBarContainer px={3}>
       <LogoWrapper>
         <img alt="eLife" src="/assets/elife-logo.png" />
       </LogoWrapper>
     </AppBarContainer>
-    <Centerer>{children}</Centerer>
+    <SmallCenterer>{children}</SmallCenterer>
   </Box>
 )
 
--- app/components/pages/LandingRedirect/LandingRedirect.js
+++ app/components/pages/LandingRedirect/LandingRedirect.js
@@ -1,9 +1,43 @@
 import React from 'react'
-import { RedirectLayout } from '../../global'
+import styled from 'styled-components'
+import { Box } from '@rebass/grid'
+import { H1 } from '@pubsweet/ui'
+import Paragraph from 'ui/atoms/Paragraph'
+import ButtonLink from 'ui/atoms/ButtonLink'
+import { RedirectLayout, TwoColumnLayout } from '../../global'
+
+const ImageBlock = styled(Box)`
+  width:100%
+  height: 100%;
+  background-image: url("/assets/redirect.png");
+`
 
 const LandingRedirect = () => (
   <RedirectLayout>
-    <span>Hello World</span>
+    <TwoColumnLayout>
+      <Box>
+        <Box mb={4}>
+          <H1>We&apos;re redirecting you</H1>
+        </Box>
+        <Box mb={5}>
+          <Paragraph.Writing>
+            eLife is working on a new submission system designed to make the
+            process of publishing your research faster and simpler.
+          </Paragraph.Writing>
+          <Paragraph.Writing>
+            We&apos;re redirecting you to our new system, which will guide you
+            through the submission process.
+          </Paragraph.Writing>
+          <Paragraph.Writing>
+            You may be asked to login using ORCID.
+          </Paragraph.Writing>
+        </Box>
+        <ButtonLink primary to="/">
+          CONTINUE
+        </ButtonLink>
+      </Box>
+      <ImageBlock />
+    </TwoColumnLayout>
   </RedirectLayout>
 )
 
