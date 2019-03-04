0ea6bc8f51d3b7328b119500e3a74eb078dc31b2
Semra Shukrieva <semra.shukrieva@yld.io>
2019-03-04 15:40:16 +0000

    move scienceBeamApi to xpub-client folder

--- server/xpub-controller/index.js
+++ server/xpub-controller/index.js
@@ -1,11 +1,11 @@
 const SupportingFiles = require('./supportingFiles')
 const Manuscript = require('./manuscript')
 const S3Storage = require('./client/s3Storage')
-const ScienceBeamApi = require('./client/scienceBeamApi')
+const ScienceBeamApi = require('../xpub-client/scienceBeamApi')
 
 module.exports = {
   SupportingFiles,
   Manuscript,
   S3Storage,
-  ScienceBeamApi
+  ScienceBeamApi,
 }
