a7a5ee6e6f673a813c99737d7cb75963e10432ab
jyenes <javier.yenes@yld.io>
2019-03-05 17:12:57 +0000

    unifying suppressions checks in one test, which performs the checks on the fly

--- test/hotjar.browser.js
+++ test/hotjar.browser.js
@@ -23,20 +23,17 @@ const manuscript = {
   ],
 }
 
-test('test suppression is in place for the name', async t => {
+test('test suppressions', async t => {
+  const { server } = await startSshServer(
+    config.get('meca.sftp.connectionOptions.port'),
+  )
+
   const navigationHelper = new NavigationHelper(t)
 
   navigationHelper.login()
-  navigationHelper.wait(1000)
   navigationHelper.openProfile()
 
   await t.expect(profile.name, { 'data-hj-suppress': '' }).ok()
-})
-
-test('test suppression is in place for the coverletter', async t => {
-  const navigationHelper = new NavigationHelper(t)
-
-  navigationHelper.login()
   navigationHelper.newSubmission()
 
   // author's page
@@ -50,24 +47,6 @@ test('test suppression is in place for the coverletter', async t => {
       'data-hj-suppress': '',
     })
     .ok()
-})
-
-test('test disclousure is suppressing name', async t => {
-  const { server } = await startSshServer(
-    config.get('meca.sftp.connectionOptions.port'),
-  )
-
-  const navigationHelper = new NavigationHelper(t)
-
-  navigationHelper.login()
-  navigationHelper.newSubmission()
-
-  // author's page
-  navigationHelper.preFillAuthorDetailsWithOrcid()
-  navigationHelper.setAuthorEmail('example@example.org')
-  navigationHelper.navigateForward()
-
-  // files' page
   navigationHelper.fillCoverletter('\nPlease consider this for publication')
   navigationHelper.uploadManuscript(manuscript)
   navigationHelper.wait(1000)
@@ -105,32 +84,23 @@ test('test disclousure is suppressing name', async t => {
       'data-hj-suppress': '',
     })
     .ok()
-
-  await new Promise((resolve, reject) =>
-    server.close(err => (err ? reject(err) : resolve())),
-  )
-})
-
-test('checking suppresion on dashboard once a submission has finished', async t => {
-  const navigationHelper = new NavigationHelper(t)
-  await navigationHelper.fullSubmission(manuscript)
-  await navigationHelper.wait(1000)
-  await t
-    .expect(dashboard.titles, {
-      'data-hj-suppress': '',
-    })
-    .ok()
-})
-
-test('checking suppresion on the thankyou page', async t => {
-  const navigationHelper = new NavigationHelper(t)
-  await navigationHelper.paricialSubmissionThankyou(manuscript)
-  await navigationHelper.consentDisclosure()
-  await navigationHelper.submit()
-  await navigationHelper.accept()
+  navigationHelper.consentDisclosure()
+  navigationHelper.submit()
+  navigationHelper.accept()
   await t
     .expect(thankyou.title, {
       'data-hj-suppress': '',
     })
     .ok()
+
+  navigationHelper.thankyou()
+  await t
+    .expect(dashboard.titles, {
+      'data-hj-suppress': '',
+    })
+    .ok()
+
+  await new Promise((resolve, reject) =>
+    server.close(err => (err ? reject(err) : resolve())),
+  )
 })
