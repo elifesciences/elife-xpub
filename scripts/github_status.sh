21dec6915d163610244e4493cb6a3f702e7d4242
Semra Shukrieva <semra.shukrieva@yld.io>
2019-03-05 16:10:43 +0000

    test: try setting up codecov

--- scripts/github_status.sh
+++ scripts/github_status.sh
@@ -8,3 +8,4 @@ version=${CI_COMMIT_SHA};
 apiUrl=https://api.github.com/repos/elifesciences/elife-xpub/statuses/${version}
 postData='{"state": "'$status'", "target_url": "'${targetUrl}'", "context": "'${context}'", "description": "'${description}'"}'
 curl -u gitlab-ci:$github_token_write $apiUrl -d "${postData}"
+curl -s https://codecov.io/bash | bash -s - -t token
