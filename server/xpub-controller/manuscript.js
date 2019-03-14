245a82f387e34b3f368780989f85cf2f73c9be85
jyenes <javier.yenes@yld.io>
2019-03-14 09:24:30 +0000

    adding templates and removed the dashboard notification call

--- server/xpub-controller/manuscript.js
+++ server/xpub-controller/manuscript.js
@@ -1,6 +1,5 @@
 const logger = require('@pubsweet/logger')
 const ManuscriptModel = require('@elifesciences/xpub-model').Manuscript
-const Notification = require('./notification')
 const { FilesHelper, ManuscriptHelper } = require('./helpers')
 
 class Manuscript {
@@ -83,24 +82,7 @@ class Manuscript {
       )
     }
 
-    const originalEmails = (manuscript.getAuthor()
-      ? manuscript.getAuthor()
-      : []
-    )
-      .map(author => author.alias.email)
-      .join(',')
-
     manuscript.applyInput(data)
-
-    const newAuthors = manuscript.getAuthor()
-    const newEmails = newAuthors.map(author => author.alias.email).join(',')
-
-    // Send email here only when author changes...
-    if (newEmails !== originalEmails) {
-      // sendEmail
-      const notify = new Notification(this.config, newAuthors)
-      notify.sendDashboardEmail()
-    }
 
     await manuscript.save()
     logger.debug(`Updated manuscript`, {
