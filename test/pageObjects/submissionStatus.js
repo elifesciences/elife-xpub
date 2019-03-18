e7da72c920b70fd77b19bf5f21506792c1963c0b
jyenes <javier.yenes@yld.io>
2019-03-18 20:06:07 +0000

    adding error case in the browser test

--- test/pageObjects/submissionStatus.js
+++ test/pageObjects/submissionStatus.js
@@ -5,6 +5,7 @@ const submissionStatus = {
   url: new RegExp(
     `${config.get('pubsweet-server.baseUrl')}/submissions/[a-f0-9-]{36}`,
   ),
+  loginUrl: `${config.get('pubsweet-server.baseUrl')}/login`,
   welcomeMessage: Selector('[data-test-id="welcome-message"]'),
 }
 
--- test/submissionStatus.browser.js
+++ test/submissionStatus.browser.js
@@ -1,4 +1,5 @@
 import config from 'config'
+import { ClientFunction } from 'testcafe'
 import startSshServer from '@elifesciences/xpub-meca-export/test/mock-sftp-server'
 import { submissionStatus } from './pageObjects'
 import setFixtureHooks from './helpers/set-fixture-hooks'
@@ -6,6 +7,8 @@ import NavigationHelper from './helpers/navigationHelper'
 
 const f = fixture('Test the route SubmissionStatus')
 setFixtureHooks(f)
+
+const getLocation = ClientFunction(() => document.location.href)
 
 test('Been able to see the page', async t => {
   const { server } = await startSshServer(
@@ -21,6 +24,20 @@ test('Been able to see the page', async t => {
   await t
     .expect(submissionStatus.welcomeMessage.textContent)
     .eql('we are in the status page.')
+
+  await new Promise((resolve, reject) =>
+    server.close(err => (err ? reject(err) : resolve())),
+  )
+})
+
+test('Should fail if the user is not authenticated', async t => {
+  const { server } = await startSshServer(
+    config.get('meca.sftp.connectionOptions.port'),
+  )
+
+  await t.navigateTo(submissionStatus.loginUrl)
+  await t.navigateTo('submissions/bdcd04a4-f065-48fd-b0a3-dce8eb5f4973')
+  await t.expect(getLocation()).contains(submissionStatus.loginUrl)
 
   await new Promise((resolve, reject) =>
     server.close(err => (err ? reject(err) : resolve())),
