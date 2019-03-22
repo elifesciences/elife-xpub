9545b7eb5227365b59f8d65f8a0cb57da89c2c99
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2019-03-22 10:48:10 +0000

    updated display name for client tests

--- packages/component-dashboard/config/jest.config.client.js
+++ packages/component-dashboard/config/jest.config.client.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'package-client',
+  displayName: 'component-dashboard-client',
   rootDir: '../',
   setupTestFrameworkScriptFile: '<rootDir>/config/jest-setup.client.js',
   testMatch: ['<rootDir>/client/**/*.test.js'],
