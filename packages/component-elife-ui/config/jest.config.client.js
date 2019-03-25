67469e07d39ae92a892bab90ffccaa96eabe7879
Erez Odier <erez.odier@gmail.com>
2019-03-25 14:35:59 +0000

    fixed paths to ui and global modules in ui component jest config

--- packages/component-elife-ui/config/jest.config.client.js
+++ packages/component-elife-ui/config/jest.config.client.js
@@ -6,7 +6,7 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!@?pubsweet|xpub)'],
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
-    '^ui(.*)$': '<rootDir>/../client/ui$1',
-    '^global(.*)$': '<rootDir>/../client/global$1',
+    '^ui(.*)$': '<rootDir>/client/ui$1',
+    '^global(.*)$': '<rootDir>/client/global$1',
   },
 }
