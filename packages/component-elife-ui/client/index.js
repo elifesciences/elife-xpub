905cb6deb845861bddcb22647fbfb40b59f018cf
Javier <javi.yenes@gmail.com>
2019-03-29 10:56:33 +0000

    export molecules (#1822)

--- packages/component-elife-ui/client/index.js
+++ packages/component-elife-ui/client/index.js
@@ -1,1 +1,2 @@
 export { default as atoms } from './atoms'
+export { default as molecules } from './molecules'
---
+++ packages/component-elife-ui/client/molecules/index.js
@@ -0,0 +1,5 @@
+export { default as BurgerMenu } from './BurgerMenu'
+export { default as ModalDialog } from './ModalDialog'
+export { default as ModalHistoryState } from './ModalHistoryState'
+export { default as ModalOverlay } from './ModalOverlay'
+export { default as Tabs } from './Tabs'
