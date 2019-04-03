c641692d0b5ab8af926f16fd8c26712899a8e7d9
Cory <c.donkin@elifesciences.org>
2019-04-03 09:52:01 +0100

    Remove xpub-server package

--- server/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rootDir: '../',
-  setupTestFrameworkScriptFile: '<rootDir>/server/jest-setup.js',
-  testMatch: ['<rootDir>/server/**/*.test.js'],
-  transform: {}, // turn off babel for server code
-  transformIgnorePatterns: ['/node_modules/(?!@?pubsweet)'],
-  testEnvironment: 'node',
-}
