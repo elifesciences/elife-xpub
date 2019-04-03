236d291d621e8289ba3418af12fb84f520f4f78c
Will Byrne <w.byrne@elifesciences.org>
2019-04-03 11:10:18 +0100

    echo out progress for debugging

--- scripts/pipeline-browser-tests.sh
+++ scripts/pipeline-browser-tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Started browser test script"
 set -e
-socat -d tcp-listen:10081,reuseaddr,fork tcp:localhost:10080 
+socat -d tcp-listen:10081,reuseaddr,fork tcp:localhost:10080
+echo "Started socat"
 npm run test:browser -- --screenshots /tmp/screenshots --screenshots-on-fails
+echo "Ran browser tests"
 cd packages/component-logger
+echo "CD into logger folder"
 jest --testMatch=**/log.filter-ci.js
+echo "Ran log filter test"
