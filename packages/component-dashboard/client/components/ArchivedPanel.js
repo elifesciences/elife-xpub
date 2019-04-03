c39f0a975767b111a7462ca99819ecb1d1b2bca9
Cory <c.donkin@elifesciences.org>
2019-04-03 15:32:19 +0100

    Add proptypes to ArchivePanel and a unit test

--- packages/component-dashboard/client/components/ArchivedPanel.js
+++ packages/component-dashboard/client/components/ArchivedPanel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Box } from '@rebass/grid'
 import { th } from '@pubsweet/ui-toolkit'
@@ -8,10 +9,25 @@ const EmptyListMessage = styled(Box)`
   color: ${th('colorTextSecondary')};
 `
 
-const ArchivedPanel = () => (
+const ArchivedPanel = ({ manuscripts }) => (
   <EmptyListMessage mt={7}>
-    You currently have no archived submissions
+    {manuscripts.length ? null : 'You currently have no archived submissions'}
   </EmptyListMessage>
 )
+
+ArchivedPanel.propTypes = {
+  manuscripts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      meta: PropTypes.shape({
+        title: PropTypes.string,
+      }).isRequired,
+    }),
+  ),
+}
+
+ArchivedPanel.defaultProps = {
+  manuscripts: [],
+}
 
 export default ArchivedPanel
