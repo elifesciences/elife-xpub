a066d90b386b18266fb6c0938467a2d87acb2baf
Cory <c.donkin@elifesciences.org>
2019-04-03 09:36:38 +0100

    remove xpub-server and move logging start up to component-app

--- config/default.js
+++ config/default.js
@@ -19,7 +19,6 @@ module.exports = {
       '@elifesciences/component-model-team',
       '@elifesciences/component-model-user',
       '@elifesciences/component-model',
-      '@elifesciences/xpub-server',
       '@elifesciences/component-dashboard',
       '@elifesciences/component-submission',
       '@pubsweet/component-send-email',
