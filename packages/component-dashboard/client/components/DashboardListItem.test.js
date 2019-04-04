029f64f24fabfb5d9423c4fa36aecb8f030ac2ee
Cory <corydanglia@gmail.com>
2019-04-04 09:12:44 +0100

    Refactor Dashboards client compomnents (#1881)
    * seperate graphql interface from react components
    
    * Add proptypes to ArchivePanel and a unit test
    
    * extends tests for DashboardListItem
    
    * change data attribute on dashbaord buttons

--- packages/component-dashboard/client/components/DashboardListItem.test.js
+++ packages/component-dashboard/client/components/DashboardListItem.test.js
@@ -1,6 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
 import { subDays } from 'date-fns'
 
-import { dashboardDateText } from './DashboardListItem'
+import DashboardListItem, {
+  dashboardDateText,
+  DashboardListItemContent,
+} from './DashboardListItem'
+import ManuscriptStatus from './ManuscriptStatus'
 
 describe('DashboardListItem', () => {
   describe('dashboardDateText', () => {
@@ -26,6 +32,59 @@ describe('DashboardListItem', () => {
       expectText(729, '24 months ago')
       expectText(730, '2 years ago')
       expectText(1460, '4 years ago')
+    })
+  })
+
+  describe('DashboardListItemContent', () => {
+    it('renders title, status and updated date of passed manuscript', () => {
+      const dummyManuscript = {
+        meta: {
+          title: 'MockTitle',
+        },
+        clientStatus: 'CONTINUE_SUBMISSION',
+        updated: '2019-04-03T15:24:07.190Z',
+      }
+      const wrapper = shallow(
+        <DashboardListItemContent
+          manuscript={dummyManuscript}
+          title={dummyManuscript.meta.title}
+        />,
+      )
+
+      expect(wrapper.text()).toContain('MockTitle')
+      expect(wrapper.text()).toContain('Wed 3 Apr 2019')
+      expect(wrapper.find(ManuscriptStatus)).toHaveLength(1)
+    })
+  })
+
+  describe('DashboardListItem', () => {
+    it('renders a modal wrapped component when status is not SUBMITTED', () => {
+      const dummyManuscript = {
+        meta: {
+          title: 'MockTitle',
+        },
+        clientStatus: 'CONTINUE_SUBMISSION',
+        updated: '2019-04-03T15:24:07.190Z',
+      }
+      const wrapper = shallow(
+        <DashboardListItem manuscript={dummyManuscript} onDelete={() => {}} />,
+      )
+
+      expect(wrapper.find('ModalHistoryState')).toHaveLength(1)
+    })
+    it('renders item without modal wrapper when status is SUBMITTED', () => {
+      const dummyManuscript = {
+        meta: {
+          title: 'MockTitle',
+        },
+        clientStatus: 'SUBMITTED',
+        updated: '2019-04-03T15:24:07.190Z',
+      }
+      const wrapper = shallow(
+        <DashboardListItem manuscript={dummyManuscript} onDelete={() => {}} />,
+      )
+
+      expect(wrapper.find('ModalHistoryState')).toHaveLength(0)
     })
   })
 })
