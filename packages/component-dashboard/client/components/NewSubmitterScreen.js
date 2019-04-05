eaa76be9fa4eaaf4bd65bc4b53a61ff02df31463
Cory <c.donkin@elifesciences.org>
2019-04-05 14:35:20 +0100

    Make smallcontainer an atom ui element

--- packages/component-dashboard/client/components/NewSubmitterScreen.js
+++ packages/component-dashboard/client/components/NewSubmitterScreen.js
@@ -1,27 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Box } from '@rebass/grid'
 import { TwoColumnLayout } from '@elifesciences/component-elife-ui/client/global'
-import media from '@elifesciences/component-elife-ui/client/global/layout/media'
 import {
   Paragraph,
   ImageWrapper,
   FooterPrivacy,
   NativeLink,
+  SmallCenterer,
 } from '@elifesciences/component-elife-ui/client/atoms'
 import { H1, Button } from '@pubsweet/ui'
-
-const SmallCenterer = styled(Box).attrs({
-  mx: 'auto',
-  mt: 5,
-  px: 5,
-  width: [1, 1, 1, 1000],
-})`
-  min-width: 0;
-  ${media.tabletPortraitUp`
-  margin-top: 0;
-`};
-`
 
 const NewSubmitterScreen = ({ createNewSubmission }) => (
   <SmallCenterer>
