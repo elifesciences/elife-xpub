12fe60a8c39b0ce24e58831288aee76999603815
Cory <c.donkin@elifesciences.org>
2019-04-12 16:40:29 +0100

    Move load / error logic and serialising data calls to compose wrapper. Remove SubmissionOperations

--- packages/component-submission/client/pages/SubmissionWizard.js
+++ packages/component-submission/client/pages/SubmissionWizard.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Switch, Redirect } from 'react-router-dom'
-import { compose } from 'recompose'
+import {
+  compose,
+  withHandlers,
+  withProps,
+  branch,
+  renderComponent,
+} from 'recompose'
 import { Subscription } from 'react-apollo'
 import {
   ErrorPage,
   TrackedRoute,
 } from '@elifesciences/component-elife-app/client'
+import { Loading } from '@elifesciences/component-elife-ui/client/atoms'
 
-import SubmissionOperations from '../components/SubmissionOperations'
 import AuthorPageContainer from '../components/steps/Author'
 import FilesPageContainer from '../components/steps/Files'
 import SubmissionPage from '../components/steps/Submission'
@@ -21,126 +27,140 @@ import disclosurePageSchema from '../components/steps/Disclosure/schema'
 import WizardStep from '../components/WizardStep'
 
 import withGQL from '../graphql/withGQL'
+import { parseFormToOutputData, parseInputToFormData } from '../utils'
 import { ON_UPLOAD_PROGRESS } from '../graphql/mutations'
 
 const SubmissionWizard = ({
   match,
   history,
-  data,
-  updateManuscript: updateManuscriptMutation,
-  submitManuscript: submitManuscriptMutation,
+  updateManuscript,
+  submitManuscript,
+  initialValues,
 }) => (
-  <SubmissionOperations
-    data={data}
-    submitManuscript={submitManuscriptMutation}
-    updateManuscript={updateManuscriptMutation}
+  <Subscription
+    subscription={ON_UPLOAD_PROGRESS}
+    variables={{ id: match.params.id }}
   >
-    {({ updateManuscript, submitManuscript, initialValues }) => (
-      <Subscription
-        subscription={ON_UPLOAD_PROGRESS}
-        variables={{ id: match.params.id }}
-      >
-        {({ data: uploadData, loading: uploadLoading }) => (
-          <Switch>
-            <TrackedRoute
-              exact
-              path={`${match.path}/files`}
-              render={() => (
-                <WizardStep
-                  component={FilesPageContainer}
-                  handleAutoSave={updateManuscript}
-                  handleButtonClick={updateManuscript}
-                  history={history}
-                  initialValues={initialValues}
-                  nextUrl={`${match.url}/submission`}
-                  previousUrl={`${match.url}/author`}
-                  step={1}
-                  title="Write your cover letter and upload your manuscript"
-                  uploadData={uploadData}
-                  uploadLoading={uploadLoading}
-                  validationSchema={filesPageSchema}
-                />
-              )}
+    {({ data: uploadData, loading: uploadLoading }) => (
+      <Switch>
+        <TrackedRoute
+          exact
+          path={`${match.path}/files`}
+          render={() => (
+            <WizardStep
+              component={FilesPageContainer}
+              handleAutoSave={updateManuscript}
+              handleButtonClick={updateManuscript}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`${match.url}/submission`}
+              previousUrl={`${match.url}/author`}
+              step={1}
+              title="Write your cover letter and upload your manuscript"
+              uploadData={uploadData}
+              uploadLoading={uploadLoading}
+              validationSchema={filesPageSchema}
             />
-            <TrackedRoute
-              path={`${match.path}/submission`}
-              render={() => (
-                <WizardStep
-                  component={SubmissionPage}
-                  handleAutoSave={updateManuscript}
-                  handleButtonClick={updateManuscript}
-                  history={history}
-                  initialValues={initialValues}
-                  nextUrl={`${match.url}/editors`}
-                  previousUrl={`${match.url}/files`}
-                  step={2}
-                  title="Help us get your work seen by the right people"
-                  validationSchema={submissionPageSchema}
-                />
-              )}
+          )}
+        />
+        <TrackedRoute
+          path={`${match.path}/submission`}
+          render={() => (
+            <WizardStep
+              component={SubmissionPage}
+              handleAutoSave={updateManuscript}
+              handleButtonClick={updateManuscript}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`${match.url}/editors`}
+              previousUrl={`${match.url}/files`}
+              step={2}
+              title="Help us get your work seen by the right people"
+              validationSchema={submissionPageSchema}
             />
-            <TrackedRoute
-              path={`${match.path}/editors`}
-              render={() => (
-                <WizardStep
-                  component={EditorsPageContainer}
-                  handleAutoSave={updateManuscript}
-                  handleButtonClick={updateManuscript}
-                  history={history}
-                  initialValues={initialValues}
-                  nextUrl={`${match.url}/disclosure`}
-                  previousUrl={`${match.url}/submission`}
-                  step={3}
-                  title="Who should review your work?"
-                  validationSchema={editorsPageSchema}
-                />
-              )}
+          )}
+        />
+        <TrackedRoute
+          path={`${match.path}/editors`}
+          render={() => (
+            <WizardStep
+              component={EditorsPageContainer}
+              handleAutoSave={updateManuscript}
+              handleButtonClick={updateManuscript}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`${match.url}/disclosure`}
+              previousUrl={`${match.url}/submission`}
+              step={3}
+              title="Who should review your work?"
+              validationSchema={editorsPageSchema}
             />
-            <TrackedRoute
-              path={`${match.path}/disclosure`}
-              render={() => (
-                <WizardStep
-                  component={DisclosurePage}
-                  finalStep
-                  handleAutoSave={updateManuscript}
-                  handleButtonClick={submitManuscript}
-                  history={history}
-                  initialValues={initialValues}
-                  nextUrl={`/thankyou/${match.params.id}`}
-                  previousUrl={`${match.url}/editors`}
-                  step={4}
-                  title="Disclosure of data to editors"
-                  validationSchema={disclosurePageSchema}
-                />
-              )}
+          )}
+        />
+        <TrackedRoute
+          path={`${match.path}/disclosure`}
+          render={() => (
+            <WizardStep
+              component={DisclosurePage}
+              finalStep
+              handleAutoSave={updateManuscript}
+              handleButtonClick={submitManuscript}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`/thankyou/${match.params.id}`}
+              previousUrl={`${match.url}/editors`}
+              step={4}
+              title="Disclosure of data to editors"
+              validationSchema={disclosurePageSchema}
             />
-            <TrackedRoute
-              path={`${match.path}/author`}
-              render={() => (
-                <WizardStep
-                  component={AuthorPageContainer}
-                  handleAutoSave={updateManuscript}
-                  handleButtonClick={updateManuscript}
-                  history={history}
-                  initialValues={initialValues}
-                  nextUrl={`${match.url}/files`}
-                  step={0}
-                  title="Your details"
-                  validationSchema={authorPageSchema}
-                />
-              )}
+          )}
+        />
+        <TrackedRoute
+          path={`${match.path}/author`}
+          render={() => (
+            <WizardStep
+              component={AuthorPageContainer}
+              handleAutoSave={updateManuscript}
+              handleButtonClick={updateManuscript}
+              history={history}
+              initialValues={initialValues}
+              nextUrl={`${match.url}/files`}
+              step={0}
+              title="Your details"
+              validationSchema={authorPageSchema}
             />
-            <Redirect
-              exact
-              from="/submit/:id"
-              to={`/submit/${match.params.id}/author`}
-            />
-            <ErrorPage error="404: page not found" />
-          </Switch>
-        )}
-      </Subscription>
+          )}
+        />
+        <Redirect
+          exact
+          from="/submit/:id"
+          to={`/submit/${match.params.id}/author`}
+        />
+        <ErrorPage error="404: page not found" />
+      </Switch>
     )}
-  </SubmissionOperations>
+  </Subscription>
 )
 
-export default compose(withGQL)(SubmissionWizard)
+export default compose(
+  withGQL,
+  // TODO: This error / loading branch wrapper is a common thing across app. Should be turned into an import
+  branch(
+    props => props.data && (props.data.loading && !props.data.manuscripts),
+    renderComponent(Loading),
+  ),
+  branch(props => !props.data || props.data.error, renderComponent(ErrorPage)),
+  withProps(props => ({
+    initialValues: parseInputToFormData(props.data.manuscript),
+  })),
+  withHandlers({
+    updateManuscript: props => formValues =>
+      props.updateManuscript({
+        variables: { data: parseFormToOutputData(formValues) },
+      }),
+    submitManuscript: props => formValues =>
+      props.submitManuscript({
+        variables: { data: parseFormToOutputData(formValues) },
+      }),
+  }),
+)(SubmissionWizard)
