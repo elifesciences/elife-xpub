8ef90a4037abbd591aa32ac112128efae76897f4
Peter Hooper <diversemix@gmail.com>
2019-04-16 12:05:07 +0100

    import db from new location

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,14 +1,12 @@
-import db from 'pubsweet-server/src/db'
+import { db } from '@pubsweet/db-manager'
+import replaySetup from './replay-setup'
 
 // idea: this might be another option for when running browser tests
 // while writing tests, using the following to start the server avoids having to recompile the app
 // import {startServer: start} from 'pubsweet-server'
 
-import replaySetup from './replay-setup'
-
 export async function setup(t) {
   console.log('test/helpers/setup.js::setup')
-
   console.log('test/helpers/setup.js::setup truncate query')
 
   const { rows } = await db.raw(`
