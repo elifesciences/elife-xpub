cfa42e5ecfbfeee0e253d6897e7d0284c8d36229
Cory <c.donkin@elifesciences.org>
2019-04-16 14:57:15 +0100

    Move ThankYouPage to top level pages

--- packages/component-submission/client/pages/ThankYouPage.js
+++ packages/component-submission/client/pages/ThankYouPage.js
@@ -4,7 +4,7 @@ import { Query } from 'react-apollo'
 import { Loading } from '@elifesciences/component-elife-ui/client/atoms'
 import { ErrorPage } from '@elifesciences/component-elife-app/client'
 
-import ThankYou from '../../components/ThankYou'
+import ThankYou from '../components/ThankYou'
 
 const MANUSCRIPT_TITLE = gql`
   query GetManuscript($id: ID!) {
