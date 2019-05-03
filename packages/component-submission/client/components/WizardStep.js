71a78b2756143d9f5277596a2aac4ec89cbfd259
Peter East <peter@repositive.io>
2019-05-03 10:56:30 +0100

    Make the WizardStep a stateful component

--- packages/component-submission/client/components/WizardStep.js
+++ packages/component-submission/client/components/WizardStep.js
@@ -14,82 +14,107 @@ import SavePageStatus from './SavePageStatus'
 const BoxNoMinWidth = styled(Box)`
   min-width: 0;
 `
+class WizardStep extends React.Component {
+  constructor(props) {
+    super(props)
 
-const WizardStep = ({
-  component: FormComponent,
-  finalStep,
-  handleAutoSave,
-  handleButtonClick,
-  history,
-  nextUrl,
-  previousUrl,
-  initialValues,
-  title,
-  step,
-  validationSchema,
-  ...wizardProps
-}) => (
-  <Formik
-    initialValues={initialValues}
-    // ensure each page gets a new form instance otherwise all fields are touched
-    key={title}
-    onSubmit={values => {
-      handleButtonClick(values).then(() => history.push(nextUrl))
-    }}
-    render={({
-      values,
-      handleSubmit,
-      isSubmitting,
-      setTouched,
-      submitForm,
-      validateForm,
-      ...formProps
-    }) => (
-      <Flex>
-        <BoxNoMinWidth flex="1 1 100%" mx={[0, 0, 0, '16.666%']}>
-          <form noValidate onSubmit={handleSubmit}>
-            <AutoSave
-              disabled={isSubmitting}
-              onSave={handleAutoSave}
-              values={values}
-            />
-            <SavePageStatus id={values.id} url={history.location.pathname} />
-            <Box my={5}>
-              <ProgressBar currentStep={step} />
-            </Box>
+    this.state = {
+      submissionAttempted: false,
+    }
+  }
 
-            {title && <FormH2>{title}</FormH2>}
+  setSubmissionAttempted = (value = true) => {
+    this.state.submissionAttempted = value
+  }
 
-            <FormComponent values={values} {...formProps} {...wizardProps} />
-
-            <Flex mt={6}>
-              {previousUrl && (
-                <Box mr={3}>
-                  <ButtonLink data-test-id="back" to={previousUrl}>
-                    Back
-                  </ButtonLink>
+  render() {
+    const {
+      component: FormComponent,
+      finalStep,
+      handleAutoSave,
+      handleButtonClick,
+      history,
+      nextUrl,
+      previousUrl,
+      initialValues,
+      title,
+      step,
+      validationSchema,
+      ...wizardProps
+    } = this.props
+    return (
+      <Formik
+        initialValues={initialValues}
+        // ensure each page gets a new form instance otherwise all fields are touched
+        key={title}
+        onSubmit={values => {
+          handleButtonClick(values).then(() => history.push(nextUrl))
+        }}
+        render={({
+          values,
+          handleSubmit,
+          isSubmitting,
+          setTouched,
+          submitForm,
+          validateForm,
+          ...formProps
+        }) => (
+          <Flex>
+            <BoxNoMinWidth flex="1 1 100%" mx={[0, 0, 0, '16.666%']}>
+              <form noValidate onSubmit={handleSubmit}>
+                <AutoSave
+                  disabled={isSubmitting}
+                  onSave={handleAutoSave}
+                  values={values}
+                />
+                <SavePageStatus
+                  id={values.id}
+                  url={history.location.pathname}
+                />
+                <Box my={5}>
+                  <ProgressBar currentStep={step} />
                 </Box>
-              )}
-              <Box>
-                {finalStep ? (
-                  <WizardSubmit
-                    setTouched={setTouched}
-                    submitForm={submitForm}
-                    validateForm={validateForm}
-                  />
-                ) : (
-                  <Button data-test-id="next" primary type="submit">
-                    Next
-                  </Button>
-                )}
-              </Box>
-            </Flex>
-          </form>
-        </BoxNoMinWidth>
-      </Flex>
-    )}
-    validationSchema={yup.object().shape(validationSchema)}
-  />
-)
+
+                {title && <FormH2>{title}</FormH2>}
+
+                <FormComponent
+                  values={values}
+                  {...formProps}
+                  isSubmissionAttempted={this.state.submissionAttempted}
+                  {...wizardProps}
+                />
+
+                <Flex mt={6}>
+                  {previousUrl && (
+                    <Box mr={3}>
+                      <ButtonLink data-test-id="back" to={previousUrl}>
+                        Back
+                      </ButtonLink>
+                    </Box>
+                  )}
+                  <Box>
+                    {finalStep ? (
+                      <WizardSubmit
+                        setSubmissionAttempted={this.setSubmissionAttempted}
+                        setTouched={setTouched}
+                        submitForm={submitForm}
+                        validateForm={validateForm}
+                      />
+                    ) : (
+                      <Button data-test-id="next" primary type="submit">
+                        Next
+                      </Button>
+                    )}
+                  </Box>
+                </Flex>
+              </form>
+            </BoxNoMinWidth>
+          </Flex>
+        )}
+        validationSchema={yup.object().shape(validationSchema)}
+      />
+    )
+  }
+}
 
 export default WizardStep
