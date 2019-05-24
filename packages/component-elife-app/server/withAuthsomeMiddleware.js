5ccd494be1c3006df56b5b262f2a8fcee2910adb
Peter East <petereast+gitpub@pm.me>
2019-05-24 12:16:24 +0100

    Make it possible to mock the models for policies

--- packages/component-elife-app/server/withAuthsomeMiddleware.js
+++ packages/component-elife-app/server/withAuthsomeMiddleware.js
@@ -2,11 +2,11 @@ const Authsome = require('authsome')
 const { authsomeMode } = require('./authsomeMode')
 const { AuthorizationError } = require('@pubsweet/errors')
 
-const models = require('@pubsweet/models')
+const systemModels = require('@pubsweet/models')
 
 const AUTHORIZATION_ERROR_MESSAGE = 'Operation not permitted :('
 
-const can = async (userId, verb, entity) => {
+const can = async (userId, verb, entity, models) => {
   const authsome = new Authsome({ mode: authsomeMode }, { models })
 
   const permission = await authsome.can(userId, verb, entity)
@@ -19,18 +19,27 @@ const can = async (userId, verb, entity) => {
 }
 
 const STANDARD_TYPES = ['Query', 'Mutation', 'Subscription']
-const withAuthsomeMiddleware = (resolvers = {}, useCases = {}, _can = can) =>
+const withAuthsomeMiddleware = (
+  resolvers = {},
+  useCases = {},
+  { _can, _models } = { _can: can, _models: systemModels },
+) =>
   Object.entries(resolvers).reduce(
     (acc, [gqlType, gqlResolvers]) => ({
       ...acc,
       [gqlType]: STANDARD_TYPES.includes(gqlType)
-        ? injectMiddleware(gqlResolvers, useCases, _can)
+        ? injectMiddleware(gqlResolvers, useCases, _can, _models)
         : gqlResolvers,
     }),
     {},
   )
 
-const injectMiddleware = (gqlResolvers, useCases, _can = can) =>
+const injectMiddleware = (
+  gqlResolvers,
+  useCases,
+  _can = can,
+  _models = systemModels,
+) =>
   Object.entries(gqlResolvers).reduce(
     (acc, [name, originalResolver]) => ({
       ...acc,
@@ -42,6 +51,7 @@ const injectMiddleware = (gqlResolvers, useCases, _can = can) =>
             policies: useCases[`${name}UseCase`].authsomePolicies,
           },
           params,
+          _models,
         )
 
         // NOTE: This will throw if the conditions don't pass so no need
