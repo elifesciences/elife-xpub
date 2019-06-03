111848741dbd5fc18d14bb553512841603f1e609
Will Byrne <w.byrne@elifesciences.org>
2019-06-03 16:21:35 +0100

    use hooks to manage progress bar state and update react-dom

--- packages/component-submission/client/pages/NewSubmissionWizard.js
+++ packages/component-submission/client/pages/NewSubmissionWizard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { compose, withProps, branch, renderComponent } from 'recompose'
 import { Switch, Redirect } from 'react-router-dom'
 import { Box, Flex } from '@rebass/grid'
@@ -20,56 +20,65 @@ const BoxNoMinWidth = styled(Box)`
   min-width: 0;
 `
 
-const NewSubmissionWizard = ({ initialValues, match, history }) => (
-  <Formik
-    initialValues={initialValues}
-    render={formikProps => (
-      <Flex>
-        <BoxNoMinWidth flex="1 1 100%" mx={[0, 0, 0, '16.666%']}>
-          <Box my={5}>
-            <ProgressBar
-              currentStep={Object.keys(STEP_NAMES)
-                .map(step => STEP_NAMES[step].toLowerCase())
-                .indexOf(history.location.pathname.split('/')[3])}
-            />
-          </Box>
-          <Switch>
-            <TrackedRoute
-              formikProps={formikProps}
-              path={`${match.path}/author`}
-              render={() => <AuthorStep {...formikProps} />}
-            />
-            <TrackedRoute
-              formikProps={formikProps}
-              path={`${match.path}/files`}
-              render={() => <div>file step</div>}
-            />
-            <TrackedRoute
-              formikProps={formikProps}
-              path={`${match.path}/details`}
-              render={() => <div>details step</div>}
-            />
-            <TrackedRoute
-              formikProps={formikProps}
-              path={`${match.path}/editors`}
-              render={() => <div>editors step</div>}
-            />
-            <TrackedRoute
-              formikProps={formikProps}
-              path={`${match.path}/disclosure`}
-              render={() => <div>disclosure step</div>}
-            />
-            <Redirect
-              from="/submit/:id"
-              to={`/newSubmit/${match.params.id}/author`}
-            />
-            <ErrorPage error="404: page not found" />
-          </Switch>
-        </BoxNoMinWidth>
-      </Flex>
-    )}
-  />
-)
+const NewSubmissionWizard = ({ initialValues, match, history }) => {
+  const [currentStep, setCurrentStep] = useState(0)
+
+  useEffect(() => {
+    const newStep = Object.keys(STEP_NAMES)
+      .map(step => STEP_NAMES[step].toLowerCase())
+      .indexOf(history.location.pathname.split('/')[3])
+    if (newStep !== currentStep) {
+      setCurrentStep(newStep)
+    }
+  })
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      render={formikProps => (
+        <Flex>
+          <BoxNoMinWidth flex="1 1 100%" mx={[0, 0, 0, '16.666%']}>
+            <Box my={5}>
+              <ProgressBar currentStep={currentStep} />
+            </Box>
+            <Switch>
+              <TrackedRoute
+                formikProps={formikProps}
+                path={`${match.path}/author`}
+                render={() => <AuthorStep {...formikProps} />}
+              />
+              <TrackedRoute
+                formikProps={formikProps}
+                path={`${match.path}/files`}
+                render={() => <div>file step</div>}
+              />
+              <TrackedRoute
+                formikProps={formikProps}
+                path={`${match.path}/details`}
+                render={() => <div>details step</div>}
+              />
+              <TrackedRoute
+                formikProps={formikProps}
+                path={`${match.path}/editors`}
+                render={() => <div>editors step</div>}
+              />
+              <TrackedRoute
+                formikProps={formikProps}
+                path={`${match.path}/disclosure`}
+                render={() => <div>disclosure step</div>}
+              />
+              <Redirect
+                from="/submit/:id"
+                to={`/newSubmit/${match.params.id}/author`}
+              />
+              <ErrorPage error="404: page not found" />
+            </Switch>
+          </BoxNoMinWidth>
+        </Flex>
+      )}
+    />
+  )
+}
 
 export default compose(
   wizardWithGQL,
