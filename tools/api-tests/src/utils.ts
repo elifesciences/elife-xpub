8b281258de467b77f0f7e6bf259bd89ebdd18ce3
Peter East <petereast+gpg@pm.me>
2019-06-04 16:57:41 +0100

    Add helper method to authenticate with a token

--- tools/api-tests/src/utils.ts
+++ tools/api-tests/src/utils.ts
@@ -1,6 +1,18 @@
+import {ApiTestContext} from './xpub-api';
+import { GraphQLClient } from 'graphql-request';
+
 export class NotImplementedError extends Error {
   constructor(message: string = 'not implemented') {
     super(message);
     this.name = 'NotImplementedError';
   }
+}
+
+export async function withAuthorization(config: ApiTestContext, query: string) {
+  const gqlClient = new GraphQLClient(config.state.connection.graphql_url, {
+    headers: {Authorization: config.state.connection.authorization.getOrElse("")}
+  });
+
+  return await gqlClient.request(query);
+
 }
