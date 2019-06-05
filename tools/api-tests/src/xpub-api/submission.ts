76e75baf8d60a86ff0c08b81eac8a50a8aa9d6e6
Peter East <petereast+gpg@pm.me>
2019-06-05 15:35:30 +0100

    Add crud operations for Manuscripts

--- tools/api-tests/src/xpub-api/submission.ts
+++ tools/api-tests/src/xpub-api/submission.ts
@@ -2,18 +2,15 @@
  * Helpers for component-submission
  */
 
-import { ApiTestContext, GqlChunk } from "./index";
+import { ApiTestContext } from "./index";
 // import { GraphQLClient} from 'graphql-request';
-import { Manuscript, EditorAlias } from "../generated/graphql";
+import { Manuscript, MutationUpdateManuscriptArgs, EditorAlias } from "../generated/graphql";
 import { withAuthorization, NotImplementedError } from "../utils";
 
 /**
  * Gets a manuscript
  */
-const manuscript = async (ctx: ApiTestContext): Promise<Manuscript> => {
-  const query = ``;
-
-  return await withAuthorization(ctx, query);
+const manuscript = async (_ctx: ApiTestContext): Promise<Manuscript> => {
   throw new NotImplementedError();
 };
 
@@ -27,8 +24,91 @@ const editors = async (_ctx: ApiTestContext): Promise<Array<EditorAlias>> => {
 /**
  * Updates or creates a manuscript
  */
-const updateManuscript = async (_ctx: ApiTestContext, data: Manuscript): Promise<Manuscript> => {
-  console.log(data);
+const updateManuscript = async (ctx: ApiTestContext, data: MutationUpdateManuscriptArgs ): Promise<{updateManuscript: Manuscript}> => {
+  const query = `
+mutation UpdateManuscript($data: ManuscriptInput!) {
+  updateManuscript(data: $data) {
+    ...WholeManuscript
+    __typename
+  }
+}
+fragment WholeManuscript on Manuscript {
+  id
+  clientStatus
+  meta {
+    title
+    articleType
+    subjects
+    __typename
+  }
+  lastStepVisited
+  suggestedSeniorEditors: assignees(role: "suggestedSeniorEditor") {
+    ...EditorDetails
+    __typename
+  }
+  opposedSeniorEditors: assignees(role: "opposedSeniorEditor") {
+    ...EditorDetails
+    __typename
+  }
+  opposedSeniorEditorsReason
+  suggestedReviewingEditors: assignees(role: "suggestedReviewingEditor") {
+    ...EditorDetails
+    __typename
+  }
+  opposedReviewingEditors: assignees(role: "opposedReviewingEditor") {
+    ...EditorDetails
+    __typename
+  }
+  opposedReviewingEditorsReason
+  suggestedReviewers: assignees(role: "suggestedReviewer") {
+    ...ReviewerDetails
+    __typename
+  }
+  opposedReviewers: assignees(role: "opposedReviewer") {
+    ...ReviewerDetails
+    __typename
+  }
+  opposedReviewersReason
+  files {
+    downloadLink
+    filename
+    type
+    status
+    id
+    __typename
+  }
+  coverLetter
+  author {
+    firstName
+    lastName
+    email
+    aff
+    __typename
+  }
+  previouslyDiscussed
+  previouslySubmitted
+  cosubmission
+  submitterSignature
+  disclosureConsent
+  fileStatus
+  __typename
+}
+fragment EditorDetails on EditorAlias {
+  id
+  name
+  aff
+  focuses
+  expertises
+  __typename
+}
+fragment ReviewerDetails on ReviewerAlias {
+  name
+  email
+  __typename
+}
+  `;
+
+  return await withAuthorization(ctx, query, data );
   throw new NotImplementedError();
 };
 
@@ -74,7 +154,7 @@ const fileUploadProgress = async (_ctx: ApiTestContext): Promise<Manuscript> => 
   throw new NotImplementedError();
 };
 
-const resolvers: GqlChunk = {
+const resolvers = {
   Query: { manuscript, editors },
   Mutation: {
     updateManuscript,
