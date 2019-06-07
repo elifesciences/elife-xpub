82ad2c044733f7380ab462c593e39b11f1e5271b
Cory <c.donkin@elifesciences.org>
2019-06-07 10:42:56 +0100

    use a class component as fucntional don't work well with set intervals

--- packages/component-submission/client/components/SubmissionSave.js
+++ packages/component-submission/client/components/SubmissionSave.js
@@ -1,40 +1,48 @@
-import { useState, useEffect } from 'react'
+import React from 'react'
 import { omit, isEqual, cloneDeep } from 'lodash'
 import { FORM_FIELDS_TO_OMIT } from '../utils/constants'
 
-// reference to props being saved (see below)
-let scopedProps
-
-const SubmissionSave = props => {
-  const [previousValues, setPreviousValues] = useState(cloneDeep(props.values))
-  const [isSaving, setIsSaving] = useState(false)
-
-  // updated scopedProps with new props
-  // this is needed as when calling setInterval, the save method
-  // from the first render would be called with the initial props object
-  scopedProps = props
-
-  useEffect(() => {
-    const timer = setInterval(save, 10000)
-    return () => clearInterval(timer)
-  }, [])
-
-  function save() {
-    const filteredPreviousValues = omit(previousValues, FORM_FIELDS_TO_OMIT)
-    const filteredCurrentValues = omit(scopedProps.values, FORM_FIELDS_TO_OMIT)
-
-    if (!isSaving && !isEqual(filteredPreviousValues, filteredCurrentValues)) {
-      setPreviousValues(scopedProps.values)
-      setIsSaving(true)
-
-      props
-        .onUpdate(filteredCurrentValues)
-        .then(() => setIsSaving(false))
-        .catch(() => setIsSaving(false))
+class SubmissionSave extends React.Component {
+  constructor(props) {
+    super(props)
+    this.previousValues = cloneDeep(props.values)
+    this.state = {
+      isSaving: false,
     }
   }
 
-  return null
+  componentDidMount() {
+    this.timer = setInterval(this.save, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  save = () => {
+    const filteredPreviousValues = omit(
+      this.previousValues,
+      FORM_FIELDS_TO_OMIT,
+    )
+    const filteredCurrentValues = omit(this.props.values, FORM_FIELDS_TO_OMIT)
+
+    if (
+      !this.state.isSaving &&
+      !isEqual(filteredPreviousValues, filteredCurrentValues)
+    ) {
+      this.previousValues = cloneDeep(this.props.values)
+      this.setState({ isSaving: true })
+
+      this.props
+        .onUpdate(filteredCurrentValues)
+        .then(() => this.setState({ isSaving: false }))
+        .catch(() => this.setState({ isSaving: false }))
+    }
+  }
+
+  render() {
+    return null
+  }
 }
 
 export default SubmissionSave
