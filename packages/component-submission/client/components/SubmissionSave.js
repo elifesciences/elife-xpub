5f1ffe4484078c5809533e7e71c2c314098bb845
Cory <c.donkin@elifesciences.org>
2019-06-07 10:13:18 +0100

    get interval saving working

--- packages/component-submission/client/components/SubmissionSave.js
+++ packages/component-submission/client/components/SubmissionSave.js
@@ -2,30 +2,39 @@ import { useState, useEffect } from 'react'
 import { omit, isEqual, cloneDeep } from 'lodash'
 import { FORM_FIELDS_TO_OMIT } from '../utils/constants'
 
+// reference to props being saved (see below)
+let scopedProps
+
 const SubmissionSave = props => {
   const [previousValues, setPreviousValues] = useState(cloneDeep(props.values))
   const [isSaving, setIsSaving] = useState(false)
-  let timer
+
+  // updated scopedProps with new props
+  // this is needed as when calling setInterval, the save method
+  // from the first render would be called with the initial props object
+  scopedProps = props
 
   useEffect(() => {
-    timer = setInterval(() => save, 5000)
+    const timer = setInterval(save, 10000)
     return () => clearInterval(timer)
   }, [])
 
   function save() {
     const filteredPreviousValues = omit(previousValues, FORM_FIELDS_TO_OMIT)
-    const filteredCurrentValues = omit(props.values, FORM_FIELDS_TO_OMIT)
+    const filteredCurrentValues = omit(scopedProps.values, FORM_FIELDS_TO_OMIT)
 
     if (!isSaving && !isEqual(filteredPreviousValues, filteredCurrentValues)) {
-      setPreviousValues(props.values)
+      setPreviousValues(scopedProps.values)
       setIsSaving(true)
 
       props
         .onUpdate(filteredCurrentValues)
         .then(() => setIsSaving(false))
-        .then(() => setIsSaving(false))
+        .catch(() => setIsSaving(false))
     }
   }
+
+  return null
 }
 
 export default SubmissionSave
