97aac5b48b3b90357662c071c4128c9e880a0f7f
Cory <c.donkin@elifesciences.org>
2019-06-07 15:17:24 +0100

    fix broken test prop

--- packages/component-submission/client/components/SubmissionSave.test.js
+++ packages/component-submission/client/components/SubmissionSave.test.js
@@ -8,58 +8,60 @@ jest.useFakeTimers()
 const makeWrapper = props =>
   shallow(
     <SubmissionSave
-      disabled={false}
-      handleSave={jest.fn()}
-      values={{ a: 1 }}
-      {...props}
+      {...{
+        disabled: false,
+        handleSave: jest.fn(),
+        values: { a: 1 },
+        ...props,
+      }}
     />,
   )
 
 describe('SubmissionSave', () => {
   it('must not save when disabled', () => {
-    const onUpdate = jest.fn()
-    const wrapper = makeWrapper({ onUpdate, disabled: true })
+    const handleSave = jest.fn()
+    const wrapper = makeWrapper({ handleSave, disabled: true })
 
     wrapper.setProps({ values: { a: 2 } })
     jest.runOnlyPendingTimers()
-    expect(onUpdate).not.toHaveBeenCalled()
+    expect(handleSave).not.toHaveBeenCalled()
   })
 
   it('must save when values change', () => {
-    const onUpdate = jest.fn(() => Promise.resolve())
-    const wrapper = makeWrapper({ onUpdate })
+    const handleSave = jest.fn(() => Promise.resolve())
+    const wrapper = makeWrapper({ handleSave })
 
     wrapper.setProps({ values: { a: 2 } })
     jest.runOnlyPendingTimers()
-    expect(onUpdate).toHaveBeenCalledTimes(1)
-    expect(onUpdate.mock.calls[0]).toEqual([{ a: 2 }])
+    expect(handleSave).toHaveBeenCalledTimes(1)
+    expect(handleSave.mock.calls[0]).toEqual([{ a: 2 }])
   })
 
   it('must not save when values do not change', () => {
-    const onUpdate = jest.fn()
-    const wrapper = makeWrapper({ onUpdate, disabled: true })
+    const handleSave = jest.fn()
+    const wrapper = makeWrapper({ handleSave, disabled: true })
 
     wrapper.setProps({ values: { a: 1 } })
     jest.runOnlyPendingTimers()
-    expect(onUpdate).not.toHaveBeenCalled()
+    expect(handleSave).not.toHaveBeenCalled()
   })
 
   it('must not save when its still saving', () => {
-    const onUpdate = jest.fn(() => new Promise((resolve, reject) => {}))
-    const wrapper = makeWrapper({ onUpdate })
+    const handleSave = jest.fn(() => new Promise((resolve, reject) => {}))
+    const wrapper = makeWrapper({ handleSave })
 
     wrapper.setProps({ values: { a: 2 } })
     jest.runOnlyPendingTimers()
     wrapper.setProps({ values: { a: 3 } })
     jest.runOnlyPendingTimers()
 
-    expect(onUpdate).toHaveBeenCalledTimes(1)
-    expect(onUpdate.mock.calls[0]).toEqual([{ a: 2 }])
+    expect(handleSave).toHaveBeenCalledTimes(1)
+    expect(handleSave.mock.calls[0]).toEqual([{ a: 2 }])
   })
 
   it('must save after a successful save', async () => {
-    const onUpdate = jest.fn(() => Promise.resolve())
-    const wrapper = makeWrapper({ onUpdate })
+    const handleSave = jest.fn(() => Promise.resolve())
+    const wrapper = makeWrapper({ handleSave })
 
     wrapper.setProps({ values: { a: 2 } })
     jest.runOnlyPendingTimers()
@@ -69,14 +71,14 @@ describe('SubmissionSave', () => {
     jest.runOnlyPendingTimers()
     await flushPromises()
 
-    expect(onUpdate).toHaveBeenCalledTimes(2)
-    expect(onUpdate.mock.calls[0]).toEqual([{ a: 2 }])
-    expect(onUpdate.mock.calls[1]).toEqual([{ a: 3 }])
+    expect(handleSave).toHaveBeenCalledTimes(2)
+    expect(handleSave.mock.calls[0]).toEqual([{ a: 2 }])
+    expect(handleSave.mock.calls[1]).toEqual([{ a: 3 }])
   })
 
   it('must save after a failed save', async () => {
-    const onUpdate = jest.fn(() => Promise.reject())
-    const wrapper = makeWrapper({ onUpdate })
+    const handleSave = jest.fn(() => Promise.reject())
+    const wrapper = makeWrapper({ handleSave })
 
     wrapper.setProps({ values: { a: 2 } })
     jest.runOnlyPendingTimers()
@@ -86,8 +88,8 @@ describe('SubmissionSave', () => {
     jest.runOnlyPendingTimers()
     await flushPromises()
 
-    expect(onUpdate).toHaveBeenCalledTimes(2)
-    expect(onUpdate.mock.calls[0]).toEqual([{ a: 2 }])
-    expect(onUpdate.mock.calls[1]).toEqual([{ a: 3 }])
+    expect(handleSave).toHaveBeenCalledTimes(2)
+    expect(handleSave.mock.calls[0]).toEqual([{ a: 2 }])
+    expect(handleSave.mock.calls[1]).toEqual([{ a: 3 }])
   })
 })
