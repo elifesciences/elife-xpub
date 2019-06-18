4b9a773de68d8383ad03e18e40b051b097a6a0e4
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-18 15:18:07 +0100

    Go all-in with S3 charts

--- scripts/helm_deploy.sh
+++ scripts/helm_deploy.sh
@@ -11,23 +11,4 @@ fi
 release_name="elife-xpub--${1}"
 image_tag="${2}"
 
-# temporary: should use an S3 chart repository
-mkdir -p tmp
-if [ ! -d tmp/elife-xpub-formula ]; then
-    git clone git@github.com:elifesciences/elife-xpub-formula.git tmp/elife-xpub-formula
-    cd tmp/elife-xpub-formula
-else
-    cd tmp/elife-xpub-formula
-    # workaround for https://github.com/elifesciences/issues/issues/4634
-    git reset --hard
-    git pull origin master
-fi
-
-cd helm/
-cd elife-xpub
-# workaround to allow the `elife` user to move around files into a `jenkins` folder
-chmod 777 .
-sudo -u elife -H helm init --client-only
-sudo -u elife -H helm dependency update .
-cd -
-sudo -u elife -H helm upgrade --install "$release_name" --set image.tag="${image_tag}" elife-xpub
+sudo -u elife -H helm upgrade --install "$release_name" --set image.tag="${image_tag}" alfred/elife-xpub
