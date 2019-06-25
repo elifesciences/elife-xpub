54d465e6149bf6184bcaa031963434ec07a45722
Cory <c.donkin@elifesciences.org>
2019-06-25 08:57:16 +0100

    Re-name mutation being imported into withGQL
    old UPDATE_MANUSCRIPT mutation is now named updateSubmission so the import was no longer valid

--- packages/component-submission/client/graphql/withGQL.js
+++ packages/component-submission/client/graphql/withGQL.js
@@ -3,7 +3,7 @@ import { compose } from 'recompose'
 import { ALL_MANUSCRIPTS } from '@elifesciences/component-dashboard/client/graphql/queries'
 import { getSubmission } from './queries'
 import {
-  UPDATE_MANUSCRIPT,
+  updateSubmission,
   SUBMIT_MANUSCRIPT,
   UPLOAD_MANUSCRIPT_FILE,
   DELETE_MANUSCRIPT_FILE,
@@ -16,7 +16,7 @@ export default compose(
   graphql(getSubmission, {
     options: props => ({ variables: { id: props.match.params.id } }),
   }),
-  graphql(UPDATE_MANUSCRIPT, {
+  graphql(updateSubmission, {
     name: 'updateSubmission',
     refetchQueries: [{ query: ALL_MANUSCRIPTS }],
   }),
