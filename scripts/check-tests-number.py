adb623292b958b47e3d470ebbd5004164a5cb73a
Giorgio Sironi <g.sironi@elifesciences.org>
2019-07-09 10:49:51 +0100

    Upgrade syntax for Python 3

--- scripts/check-tests-number.py
+++ scripts/check-tests-number.py
@@ -9,5 +9,5 @@ if len(sys.argv) < 3:
     sys.exit(1)
 
 test_count = int(ET.parse(sys.argv[1]).getroot().attrib['tests']);
-print '%s tests have been run' % test_count
+print('%s tests have been run' % test_count)
 assert test_count > int(sys.argv[2]), 'Are you sure enough tests have been run?'
