03a2ba37bee6df60b13978e81e46a883241afb7c
Giorgio Sironi <g.sironi@elifesciences.org>
2019-07-09 10:07:20 +0100

    Add check on unit tests number

---
+++ scripts/check-test-number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import xml.etree.ElementTree as ET
+
+if len(sys.argv) < 3:
+    print("Checks a JUnit XML report contains a minimum number of tests")
+    print("Usage: %s PATH_TO_REPORT MINIMUM_NUMBER" % sys.argv[0])
+    print("Example: %s build/jest-junit/unit-tests.xml 400" % sys.argv[0])
+    sys.exit(1)
+
+test_count = int(ET.parse(sys.argv[1]).getroot().attrib['tests']);
+assert test_count > int(sys.argv[2]), 'Are you sure enough unit tests (%s) have been run?' % test_count
