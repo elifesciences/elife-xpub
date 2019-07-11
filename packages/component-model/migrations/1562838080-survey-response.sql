e28ffb77baca3176004ff74e796adbb9492d72a8
Erez Odier <erez.odier@gmail.com>
2019-07-11 11:47:56 +0100

    fixed issue with survey migration

--- packages/component-model/migrations/1562838080-survey-response.sql
+++ packages/component-model/migrations/1562838080-survey-response.sql
@@ -2,5 +2,5 @@ CREATE TABLE survey_response (
   response_id UUID PRIMARY KEY,
   submission_id UUID NOT NULL,
   response JSONB NOT NULL,
-  created TIMESTAMP WITH TIMEZONE NOT NULL DEFAULT current_timestamp; 
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
 );
