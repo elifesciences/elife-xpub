c2da8af1f05f67537c0425d364b8edbd376ca5df
Peter Hooper <diversemix@gmail.com>
2019-07-12 13:11:50 +0100

    Remove the unused component-survey folder

--- packages/component-survey/config/jest.config.server.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  displayName: 'component-survey-server',
-  rootDir: '../',
-  setupTestFrameworkScriptFile: '<rootDir>/server/**/*.test.js',
-  transform: {}, // turn off babel for server code
-  transformIgnorePatterns: ['/node_modules/(?!@?pubsweet)'],
-  testEnvironment: 'node',
-}
