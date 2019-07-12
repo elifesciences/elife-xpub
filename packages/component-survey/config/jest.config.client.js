f060502fbe10e60192eb59b264f93d62c00043df
Peter <diversemix@gmail.com>
2019-07-12 14:05:10 +0100

    Remove the unused component-survey folder (#2311)

--- packages/component-survey/config/jest.config.client.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'component-survey-client',
-  rootDir: '../',
-  testMatch: ['<rootDir>/client/**/*.test.js'],
-  transformIgnorePatterns: ['/node_modules/(?!@?pubsweet|xpub)'],
-  moduleNameMapper: {
-    '\\.s?css$': 'identity-obj-proxy',
-  },
-}
