b83a0bd38d777754a61cfbfdeb50dfc5fb7c0643
Will Byrne <w.byrne@elifesciences.org>
2019-07-12 11:30:12 +0100

    add styling to the pages

--- packages/component-survey/client/pages/index.js
+++ packages/component-survey/client/pages/index.js
@@ -4,13 +4,14 @@ import {
   Paragraph,
 } from '@elifesciences/component-elife-ui/client/atoms'
 import { Button, TextField } from '@pubsweet/ui'
+import { Box } from '@rebass/grid'
 
 const SurveyPage = () => {
   const [country, setCountry] = useState('')
   const [gender, setGender] = useState('')
   const [timeAsPI, setTimeAsPI] = useState('')
   return (
-    <React.Fragment>
+    <Box mx="25%" width={1 / 2}>
       <FormH2>Help us reduce bias</FormH2>
       <Paragraph.Writing>
         Answering the following questions about the last author of your
@@ -18,28 +19,37 @@ const SurveyPage = () => {
         efforets to reduce bias in our submission and peer review process.
       </Paragraph.Writing>
       <Paragraph.Writing>
-        Your answers will not be shared with the Editors handling the papaer and
-        will be anonymised when we analyse the data we collect.
+        Your answers will <u>not</u> be shared with the Editors handling the
+        papaer and will be anonymised when we analyse the data we collect.
       </Paragraph.Writing>
-      <TextField
-        label="Country of residence of Last Author"
-        onChange={e => setCountry(e.value)}
-        value={country}
-      />
-      <TextField
-        label="Gender of Last Author"
-        onChange={e => setGender(e.value)}
-        value={gender}
-      />
-      <TextField
-        label="Country of residence of Last Author"
-        onChange={e => setTimeAsPI(e.value)}
-        value={timeAsPI}
-      />
-      <Button data-test-id="submit" onClick="Done" primary type="button">
-        Done
+      <Box mb={3}>
+        <TextField
+          label="Country of residence of Last Author:"
+          onChange={e => setCountry(e.target.value)}
+          placeholder="Enter text here"
+          value={country}
+        />
+      </Box>
+      <Box mb={3}>
+        <TextField
+          label="Gender of Last Author:"
+          onChange={e => setGender(e.target.value)}
+          placeholder="Enter text here"
+          value={gender}
+        />
+      </Box>
+      <Box mb={4}>
+        <TextField
+          label="When did the Last Author become an independent researcher?"
+          onChange={e => setTimeAsPI(e.target.value)}
+          placeholder="Enter text here"
+          value={timeAsPI}
+        />
+      </Box>
+      <Button data-test-id="submit" primary type="button">
+        {country === '' && gender === '' && timeAsPI === '' ? 'Skip' : 'Done'}
       </Button>
-    </React.Fragment>
+    </Box>
   )
 }
 
