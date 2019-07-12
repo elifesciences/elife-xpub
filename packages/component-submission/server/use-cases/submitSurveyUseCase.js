fab423ebc687f6a20da0860a98271b478365417e
Peter East <petereast+gpg@pm.me>
2019-07-12 10:23:24 +0100

    Generate and save a response object

--- packages/component-submission/server/use-cases/submitSurveyUseCase.js
+++ packages/component-submission/server/use-cases/submitSurveyUseCase.js
@@ -1,5 +1,30 @@
-// Export a stub use case for submit survey
+const { uniqBy, pick } = require('lodash')
 
-module.exports = (dbManager, { surveyId, answers, submissionId, userId }) =>
-  // Do some stuff eventually
-  false
+const makeResponseObject = answers =>
+  uniqBy(answers, ans => ans.questionId).reduce(
+    (acc, question) => ({
+      questions: [
+        ...(acc.questions || []),
+        pick(question, ['questionId', 'text']),
+      ],
+      answers: [
+        ...(acc.answers || []),
+        pick(question, ['questionId', 'answer']),
+      ],
+    }),
+
+    {},
+  )
+
+module.exports = ({ Survey }, { surveyId, answers, submissionId, userId }) => {
+  const response = makeResponseObject(answers)
+
+  const surveyResponse = new Survey({
+    surveyId,
+    manuscriptId: submissionId,
+    response,
+  })
+
+  surveyResponse.save()
+  return true
+}
