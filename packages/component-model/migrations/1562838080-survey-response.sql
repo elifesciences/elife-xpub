a5cdf964c3c92501be4c1c883c77922cdf2fc53c
Cory <c.donkin@elifesciences.org>
2019-07-12 08:52:20 +0100

    make survey_id a varchar column

--- packages/component-model/migrations/1562838080-survey-response.sql
+++ packages/component-model/migrations/1562838080-survey-response.sql
@@ -1,6 +1,6 @@
 CREATE TABLE survey_response (
   id UUID PRIMARY KEY,
-  survey_id VARCHAR(255) NOT NULL,
+  survey_id UUID NOT NULL,
   submission_id UUID NOT NULL,
   response JSONB NOT NULL,
   created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
