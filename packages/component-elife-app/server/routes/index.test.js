c72a63859318796654d0d0c6f3ecad27215a9a3b
Peter Hooper <diversemix@gmail.com>
2019-07-18 15:08:44 +0100

    Add 200 and 500 status code tests

--- packages/component-elife-app/server/routes/index.test.js
+++ packages/component-elife-app/server/routes/index.test.js
@@ -1,5 +1,10 @@
+jest.mock('config')
+const config = require('config')
+
+const realConfig = jest.requireActual('config')
+
 jest.mock('@pubsweet/logger')
-const { goodSymbol, badSymbol } = require('../statusPage')
+const { goodSymbol, badSymbol, disabledSymbol } = require('../statusPage')
 const express = require('express')
 const supertest = require('supertest')
 const routes = require('.')
@@ -18,12 +23,27 @@ describe('ping route test', () => {
 })
 
 describe('status route test', () => {
-  it('returns success when app ok', async () => {
+  it('returns 500 when app not ok', async () => {
     const request = makeApp()
     const result = await request.get('/status')
     expect(result.res.text).toContain(`${goodSymbol} General`)
     expect(result.res.text).toContain(`${goodSymbol} S3`)
     expect(result.res.text).toContain(`${goodSymbol} Database`)
     expect(result.res.text).toContain(`${badSymbol} SFTP`)
+    expect(result.status).toBe(500)
+  })
+
+  it('returns 200 when app not ok', async () => {
+    config.get.mockImplementation(key => {
+      if (key === 'meca.sftp.disableUpload') return true
+      return realConfig.get(key)
+    })
+    const request = makeApp()
+    const result = await request.get('/status')
+    expect(result.res.text).toContain(`${goodSymbol} General`)
+    expect(result.res.text).toContain(`${goodSymbol} S3`)
+    expect(result.res.text).toContain(`${goodSymbol} Database`)
+    expect(result.res.text).toContain(`${disabledSymbol} SFTP`)
+    expect(result.status).toBe(200)
   })
 })
