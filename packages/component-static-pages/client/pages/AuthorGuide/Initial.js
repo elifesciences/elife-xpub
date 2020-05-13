256ba565a2b796afe4d6832019ff5f9a9d2ee2bd
Hannah Drury <h.drury@elifesciences.org>
2020-05-13 14:07:03 +0100

    Update Initial.js

--- packages/component-static-pages/client/pages/AuthorGuide/Initial.js
+++ packages/component-static-pages/client/pages/AuthorGuide/Initial.js
@@ -48,7 +48,7 @@ const Initial = props => (
       <NativeLink href="https://elifesciences.org/inside-elife/d0c5d114/new-from-elife-invitation-to-submit-to-preprint-review" target="_blank">
         announcement
       </NativeLink>{' '}
-      for more details and restrictions. To opt in, please refer to the “Preprint Review” 
+      for more details and restrictions. To opt in, please refer to the &quot;Preprint Review&quot; 
       service clearly in your cover letter and include a link to the preprint.
       </Paragraph.Reading>
     </CalloutTextBox>
